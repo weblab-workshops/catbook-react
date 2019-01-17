f7a4644b16361f21f20f6b96847f7a6146b2d617
Shannen Wu <shannen@mit.edu>
2019-01-17 01:09:22 -0500

    added links

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
     constructor(props) {
@@ -10,13 +11,13 @@ class NavBar extends Component {
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="navbar-brand">Catbook</div>
                 <div className="navbar-nav">
-                    <a className="nav-item nav-link" href={"/"}>Home</a>
+                    <Link to="/" className="nav-item nav-link">Home</Link>
                         { this.props.userInfo === null ? (
-                            <a className="nav-item nav-link" href={"#"} onClick={this.props.login}>Login</a>
+                            <a className="nav-item nav-link" onClick={this.props.login}>Login</a>
                         ) : (
                             <React.Fragment>
-                                <a className="nav-item nav-link" href={"/profile/" + this.props.userInfo._id}>Profile</a>
-                                <a className="nav-item nav-link" href={"#"} onClick={this.props.logout}>Logout</a>
+                                <Link to={"/profile/" + this.props.userInfo._id} className="nav-item nav-link">Profile</Link>
+                                <a className="nav-item nav-link" onClick={this.props.logout}>Logout</a>
                             </React.Fragment>
                         )}
                 </div>
