1f037f574d192a3e8e8c950bc85821e7a7f6609c
jynnie <jynnie@mit.edu>
2019-10-30 13:40:28 -0400

    Prettify App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,62 +3,62 @@ import NavBar from "./modules/NavBar.js";
 import Feed from "./pages/Feed.js";
 import NotFound from "./pages/NotFound.js";
 import Profile from "./pages/Profile.js";
-import { Route, Switch, withRouter } from 'react-router-dom';
-import styles from "../styles.css";
+import { Route, Switch, withRouter } from "react-router-dom";
+
+import "../styles.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            userInfo: null
-        };
-    }
-
-    componentDidMount() {
-        this.getUser();
-    }
-
-	render(){
-	    return (
-	      <div>
-            <NavBar
-                userInfo={this.state.userInfo}
-                logout={this.logout}
-            />
-            <Switch>
-                <Route exact path='/' render={(props) => <Feed {...props} userInfo={this.state.userInfo} />}/>
-                <Route exact path='/profile/:user' component={Profile} />}/>
-                <Route component={NotFound} />
-            </Switch>
-        </div>
-	    );
-	}
-
-    logout = () => {
-        this.setState({
-            userInfo: null
-        })
+    this.state = {
+      userInfo: null,
     };
+  }
 
-    getUser = () => {
-        fetch('/api/whoami')
-        .then(res => res.json())
-        .then(
-            userObj => {
-                if (userObj._id !== undefined) {
-                    this.setState({
-                        userInfo: userObj
-                    });
-                } else {
-                    this.setState({
-                        userInfo: null
-                    });
-                }
-            }
-        );
-    }
+  componentDidMount() {
+    this.getUser();
+  }
 
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar userInfo={this.state.userInfo} logout={this.logout} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => <Feed {...props} userInfo={this.state.userInfo} />}
+          />
+          <Route exact path="/profile/:user" component={Profile} />
+          }/>
+          <Route component={NotFound} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+
+  logout = () => {
+    this.setState({
+      userInfo: null,
+    });
+  };
+
+  getUser = () => {
+    fetch("/api/whoami")
+      .then((res) => res.json())
+      .then((userObj) => {
+        if (userObj._id !== undefined) {
+          this.setState({
+            userInfo: userObj,
+          });
+        } else {
+          this.setState({
+            userInfo: null,
+          });
+        }
+      });
+  };
 }
 
 export default withRouter(App);
