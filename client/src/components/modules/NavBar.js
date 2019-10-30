c41f27dd57a28c16739fd8284509830b007de988
jynnie <jynnie@mit.edu>
2019-10-30 14:02:46 -0400

    Created styles for NavBar

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,29 +1,39 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import "./NavBar.css";
 
 class NavBar extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div className="navbar-brand">Catbook</div>
-                <div className="navbar-nav">
-                    <Link to="/" className="nav-item nav-link">Home</Link>
-                        { this.props.userInfo === null ? (
-                            <a className="nav-item nav-link" href="/auth/google">Login</a>
-                        ) : (
-                            <React.Fragment>
-                                <Link to={"/profile/" + this.props.userInfo._id} className="nav-item nav-link">Profile</Link>
-                                <a className="nav-item nav-link" href="/logout" onClick={this.props.logout}>Logout</a>
-                            </React.Fragment>
-                        )}
-                </div>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="NavBar-container">
+        <div className="NavBar-title">Catbook</div>
+        <div className="NavBar-linkContainer">
+          <Link to="/" className="NavBar-link">
+            Home
+          </Link>
+          {this.props.userInfo === null ? (
+            <a className="NavBar-link" href="/auth/google">
+              Login
+            </a>
+          ) : (
+            <React.Fragment>
+              <Link to={"/profile/" + this.props.userInfo._id} className="NavBar-link">
+                Profile
+              </Link>
+              <a className="NavBar-link" href="/logout" onClick={this.props.logout}>
+                Logout
+              </a>
+            </React.Fragment>
+          )}
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
