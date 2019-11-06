12e8feeec78be140bac51f30073282189b81f134
Alex Chen <achen19@mit.edu>
2019-11-06 18:51:18 -0500

    upgrade node

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,56 @@
-const path = require('path');
-const entryFile = path.resolve(__dirname,'client', 'src', 'index.js');
-const outputDir = path.resolve(__dirname, 'client', 'dist');
+const path = require("path");
+const entryFile = path.resolve(__dirname, "client", "src", "index.js");
+const outputDir = path.resolve(__dirname, "client", "dist");
 
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = {
-  entry: ['@babel/polyfill', entryFile],
+  entry: ["@babel/polyfill", entryFile],
   output: {
     path: outputDir,
-    publicPath: '/',
-    filename: 'bundle.js'
+    publicPath: "/",
+    filename: "bundle.js",
   },
   module: {
-
     rules: [
       {
         test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        loader: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.(scss|css)$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: "style-loader",
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
           },
-          {
-            loader: 'sass-loader'
-          }
-        ]
+        ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           {
-            loader: 'url-loader'
-          }
-        ]
-      }
-    ]
+            loader: "url-loader",
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ["*", ".js", ".jsx"],
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
     historyApiFallback: true,
-    contentBase: './client/dist',
+    contentBase: "./client/dist",
     hot: true,
     proxy: {
-      '/api': 'http://localhost:3000',
-      '/auth': 'http://localhost:3000',
-      '/logout': 'http://localhost:3000'
-    }
-  }
+      "/api": "http://localhost:3000",
+      "/auth": "http://localhost:3000",
+      "/logout": "http://localhost:3000",
+    },
+  },
 };
