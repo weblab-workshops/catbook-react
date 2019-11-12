350871aad869f15b9e5c1f8034f7241e777d5232
jynnie <jynnie@mit.edu>
2019-11-12 14:10:35 -0500

    NavBar docs

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -3,6 +3,9 @@ import { Link } from "@reach/router";
 
 import "./NavBar.css";
 
+/**
+ * The navigation bar at the top of all pages. Takes no props.
+ */
 class NavBar extends Component {
   constructor(props) {
     super(props);
