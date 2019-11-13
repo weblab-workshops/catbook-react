6e9609bf4a409f204c465fc01e3ac7fa0a18c6de
Nikhil Singhal <niks@mit.edu>
2019-11-13 03:40:30 -0500

    add comments and debug messages for get/post

--- client/src/utilities.js
+++ client/src/utilities.js
@@ -13,17 +13,43 @@ function formatParams(params) {
     .join("&");
 }
 
-// Helper code to make a get request. Default parameter of empty JSON Object for params.
-export function get(endpoint, params = {}) {
-  const fullPath = endpoint + "?" + formatParams(params);
-  return fetch(fullPath).then((res) => res.json());
+// convert a fetch result to a JSON object with error handling for fetch and json errors
+function convertToJSON(res) {
+  if(!res.ok) {
+    throw `API request failed with response status ${res.status} and text: ${res.statusText}`;
+  }
+  return res.clone() // clone so that the original is still readable for debugging
+      .json() // start converting to JSON object
+      .catch(error => {
+        // throw an error containing the text that couldn't be converted to JSON
+        return res.text().then(text => {
+          throw `API request's result could not be converted to a JSON object: \n${text}`;
+        });
+      });
 }
 
-// Helper code to make a post request.
-export function post(endpoint, body = {}) {
+// Helper code to make a get request. Default parameter of empty JSON Object for params.
+// Returns a Promise to a JSON Object.
+export function get(endpoint, params = {}) {
+  const fullPath = endpoint + "?" + formatParams(params);
+  return fetch(fullPath)
+    .then(convertToJSON)
+    .catch(error => {
+      // give a useful error message
+      throw `GET request to ${fullPath} failed with error:\n${error}`;
+    });
+}
+
+// Helper code to make a post request. Default parameter of empty JSON Object for params.
+// Returns a Promise to a JSON Object.
+export function post(endpoint, params = {}) {
   return fetch(endpoint, {
-    method: "post",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify(body),
-  }).then((res) => res.json());
+    method: 'post',
+    headers: {'Content-type': 'application/json'},
+    body: JSON.stringify(params)
+  }).then(convertToJSON) // convert result to JSON object
+    .catch(error => {
+      // give a useful error message
+      throw `POST request to ${endpoint} failed with error:\n${error}`;
+    });
 }
