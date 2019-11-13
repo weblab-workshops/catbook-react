bb41800f5488d179c2c73907c3755b1a8a52a75e
Aaron Sipser <ajsipser@gmail.com>
2019-11-13 02:47:07 -0500

    add header to api, and add more header to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,15 @@
 |--------------------------------------------------------------------------
 |
 | Webpack turns all the clientside HTML, CSS, Javascript into one bundle.js file.
-| Thi
+| This is done for performance reasons, as well as for compatability reasons.
+|
+| You do not have to worry about this file, except for proxy section below. 
+| All proxies does is route traffic from the hotloader to the backend.
+| You must define explicity all routes here, as we do for the /api/* routes.
+|
+| The rest of this file tell webpack which types of files to bundle (in the rules).
+| In addition, it also uses babel to transpile your javascript into code all browsers can use.
+| see https://babeljs.io/docs/en/ if this interests you!
 |
 */
 
