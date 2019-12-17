5c9126a12ea9860b2a8c7fca7f6d52a267cf0386
Alex Chen <achen19@mit.edu>
2019-12-16 19:40:10 -0500

    add cat happiness counter and small nits

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,27 +1,31 @@
 import React, { Component } from "react";
-import LatestPost from "../modules/LatestPost.js";
-import ProfilePicture from "../../public/corgi.jpg";
-
+import CatHappiness from "../modules/CatHappiness.js";
 import "../../utilities.css";
 import "./Profile.css";
 
 class Profile extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.state = {
+      catHappiness : 0,
+    }
   }
 
   componentDidMount() {
     document.title = "Profile Page";
   }
 
+  incrementCatHappiness = () => {
+    this.setState({
+      catHappiness : this.state.catHappiness + 1
+    })
+  }
+
   render() {
-    const pfpStyle = {
-      backgroundImage: `url(${ProfilePicture})`,
-    };
     return (
       <>
-        <div className="Profile-avatarContainer">
-          <div style={pfpStyle} className="Profile-avatar" />
+        <div className="Profile-avatarContainer" onClick={() => {this.incrementCatHappiness()}}>
+          <div className="Profile-avatar" />
         </div>
         <h1 className="Profile-name u-textCenter">Shannen Wu</h1>
         <hr className="Profile-line" />
@@ -33,8 +37,8 @@ class Profile extends Component {
             </div>
           </div>
           <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">My Latest Post</h4>
-            <LatestPost name="Shannen Wu" latestPost="Woof woof" id="ffffff" />
+            <h4 className="Profile-subTitle">Cat Happiness</h4>
+            <CatHappiness catHappiness={this.state.catHappiness} />
           </div>
           <div className="Profile-subContainer u-textCenter">
             <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
