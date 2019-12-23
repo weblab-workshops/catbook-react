30b03afb49deea3be1ab32cdd85e68938e553406
Alex Chen <achen19@mit.edu>
2019-12-22 21:49:53 -0800

    add empty files to workshop1

---
+++ client/src/components/modules/CatHappiness.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./CatHappiness.css";
+
+/**
+ * Component that renders cat happiness
+ *
+ * Proptypes
+ * @param {int} catHappiness is how happy your cat is
+ */
+class CatHappiness extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    // TODO: fill this in with code!
+    return ();
+  }
+}
+
+export default CatHappiness;
---
+++ client/src/components/modules/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "./NavBar.css";
+
+/**
+ * The navigation bar at the top of all pages. Takes no props.
+ */
+class NavBar extends Component {
+  render() {
+    // TODO: Implement Navbar here!
+    return (
+
+    );
+  }
+}
+
+export default NavBar;
