86e24b06eefd2564f084bc1e8f487dffaff42044
Matthew Farejowicz <mfarejow@mit.edu>
2019-12-26 14:27:41 -0600

    w2 step0

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "./modules/NavBar.js";
 import Profile from "./pages/Profile.js";
-// TODO (step0): import Feed
+import Feed from "./pages/Feed.js";
 // TODO (step5): import Router and NotFound
 
 // To use styles, import the necessary CSS files
@@ -30,8 +30,8 @@ class App extends Component {
       <>
         <NavBar />
         <div className="App-container">
-          <Profile />
-          {/* TODO (step0): render Feed instead of Profile */}
+          {/* <Profile /> */}
+          <Feed />
           {/* TODO (step5): use Router to route between pages */}
         </div>
       </>
