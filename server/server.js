2f9291e70e08789d44a1be68a18a1daa34bb5565
Cory Lynch <cjl2625@gmail.com>
2019-12-28 04:28:36 -0500

    Remove http module from server

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ const validator = require("./validator");
 validator.checkSetup();
 
 //import libraries needed for the webserver to work!
-const http = require("http"); // add http interface to node
 const bodyParser = require("body-parser"); // allow node to automatically parse POST body requests as JSON
 const express = require("express"); // backend framework for our node server.
 const session = require("express-session"); // library that stores info about each connected user
@@ -99,9 +98,6 @@ app.use((err, req, res, next) => {
 
 // hardcode port to 3000 for now
 const port = 3000;
-const server = http.Server(app);
-socket.init(server);
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
