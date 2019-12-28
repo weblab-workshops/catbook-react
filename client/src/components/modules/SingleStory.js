8675d08f3672422755d2978430540e98e6bc806d
Matthew Farejowicz <mfarejow@mit.edu>
2019-12-28 10:35:59 -0600

    add content to singlestory

--- client/src/components/modules/SingleStory.js
+++ client/src/components/modules/SingleStory.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+// TODO (step2): import Card.css
+
 /**
  * Story is a component that renders creator and content of a story
  *
@@ -18,6 +20,7 @@ class SingleStory extends Component {
   render() {
     return (
       <div className="Card-story">
+        This is a SingleStory!
         {/* TODO (step2): use JSX and props to render story creator and content */}
       </div>
     );
