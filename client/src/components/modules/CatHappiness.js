2ed32d9e9d609287ac937c7e41c495050560cd25
jynnie <jynnie@mit.edu>
2019-12-29 23:58:35 -0500

    Remove LatestPost, add CatHappiness

---
+++ client/src/components/modules/CatHappiness.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./CatHappiness.css";
+
+/**
+ * Component that renders cat happiness
+ *
+ * Proptypes
+ * @param {int} catHappiness is how happy your cat is
+ */
+class CatHappiness extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="CatHappiness-container">
+        <div className="CatHappiness-story">
+          <p className="CatHappiness-storyContent">{this.props.catHappiness}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CatHappiness;
