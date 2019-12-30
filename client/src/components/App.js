bd84315faf2442ba412b0c850d956206df51defe
Alex Chen <achen19@mit.edu>
2019-12-29 23:53:50 -0800

    new App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import NavBar from "./modules/NavBar.js";
+import Profile from "./pages/Profile.js";
 
 // to use styles, import the necessary CSS files
 import "../utilities.css";
+import "./App.css";
 
 /**
  * Define the "App" component as a class.
@@ -10,7 +13,16 @@ class App extends Component {
   // required method: whatever is returned defines what
   // shows up on screen
   render() {
-    return <div>Hello world!</div>;
+    return (
+      // <> is like a <div>, but won't show
+      // up in the DOM tree
+      <>
+        <NavBar />
+        <div className="App-container">
+          <Profile />
+        </div>
+      </>
+    );
   }
 }
 
