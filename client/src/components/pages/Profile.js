65d94db2471a630c3609e1b5ee4a172f3320b65e
Alex Chen <achen19@mit.edu>
2019-12-29 23:16:00 -0800

    remove cathappiness from step 1

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
-import CatHappiness from "../modules/CatHappiness.js";
 import "../../utilities.css";
 import "./Profile.css";
 
 class Profile extends Component {
   constructor(props) {
-    super(props)
-
+    super(props);
   }
 
   render() {
     return (
       <>
-        <div className="Profile-avatarContainer" >
+        <div className="Profile-avatarContainer">
           <div className="Profile-avatar" />
         </div>
         <h1 className="Profile-name u-textCenter">YOUR NAME HERE</h1>
@@ -23,10 +21,6 @@ class Profile extends Component {
             <div id="profile-description">
               Extra Challenge: Modify catbook to show a personalized description here!
             </div>
-          </div>
-          <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">Cat Happiness</h4>
-            <CatHappiness catHappiness={this.state.catHappiness} />
           </div>
           <div className="Profile-subContainer u-textCenter">
             <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
