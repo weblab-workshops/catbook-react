49c46f92bdb923ee7863eb24233ea6f53439c83f
Matthew Farejowicz <mfarejow@mit.edu>
2019-12-31 11:42:20 -0600

    adjust step 2 text

--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { get } from "../../utilities";
-// TODO (step3): import SingleStory
+// TODO (step2): import SingleStory
 // TODO (step4): import NewStory
 // TODO (step6): remove SingleStory import, import Card
 
@@ -18,6 +18,7 @@ class Feed extends Component {
   render() {
     return <div>This is the feed!</div>;
     // TODO (step1): render the raw stories data from state
+    // TODO (step2): render a SingleStory with hardcoded props
     // TODO (step3): map the state to SingleStory components
     // TODO (step4): add in the NewStory component
     // TODO (step6): use Card instead of SingleStory
