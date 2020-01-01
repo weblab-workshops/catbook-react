028e1ae685958024839ea97ae69a8c86d7177ffb
Kye Burchard <kyeb@mit.edu>
2020-01-01 00:46:19 -0700

    POST routes for comments/stories

--- server/api.js
+++ server/api.js
@@ -44,10 +44,33 @@ router.get("/stories", (req, res) => {
   res.send(data.stories);
 });
 
+router.post("/story", (req, res) => {
+  const newStory = {
+    _id: data.stories.length,
+    creator_name: MY_NAME,
+    content: req.body.content,
+  };
+
+  data.stories.push(newStory);
+  res.send({});
+});
+
 router.get("/comment", (req, res) => {
   // determine which comments are children of the requested story
   const filteredComments = data.comments.filter((comment) => comment.parent == req.query.parent);
   res.send(filteredComments);
+});
+
+router.post("/comment", (req, res) => {
+  const newComment = {
+    _id: data.comments.length,
+    creator_name: MY_NAME,
+    parent: req.body.parent,
+    content: req.body.content,
+  };
+
+  data.comments.push(newComment);
+  res.send({});
 });
 
 // anything else falls to this "not found" case
