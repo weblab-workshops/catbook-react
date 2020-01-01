430673a0b4dd9890e0bdc5e5d0add3056aef773d
Cory Lynch <cjl2625@gmail.com>
2019-12-31 21:45:36 -0500

    Make posts automatically show up on the screen after submit

--- server/api.js
+++ server/api.js
@@ -28,7 +28,7 @@ router.post("/story", (req, res) => {
     content: req.body.content,
   });
 
-  newStory.save().then(() => res.send({}));
+  newStory.save().then((story) => res.send(story));
 });
 
 router.get("/comment", (req, res) => {
@@ -45,5 +45,5 @@ router.post("/comment", (req, res) => {
     content: req.body.content,
   });
 
-  newComment.save().then(() => res.send({}));
+  newComment.save().then((comment) => res.send(comment));
 });
