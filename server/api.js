456ecaf4a3d5ca5b8cf4ba001cbeefb21e39776f
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-01-02 20:51:46 -0800

    removed body of GET & POST of  /stories, /story, /comment

--- server/api.js
+++ server/api.js
@@ -11,41 +11,26 @@ const express = require("express");
 
 // import models so we can interact with the database
 const Story = require("./models/story");
-const Comment = require("./models/comment");
 
 // api endpoints: all these paths will be prefixed with "/api/"
 const router = express.Router();
 
 router.get("/stories", (req, res) => {
-  // empty selector means get all documents
-  Story.find({}).then((stories) => res.send(stories));
+
 });
 
 router.post("/story", (req, res) => {
-  const newStory = new Story({
-    creator_id: req.user._id,
-    creator_name: req.user.name,
-    content: req.body.content,
-  });
 
-  newStory.save().then((story) => res.send(story));
 });
 
 router.get("/comment", (req, res) => {
-  Comment.find({ parent: req.query.parent }).then((comments) => {
+  Comment.find({ /* input parent parameter here */ }).then((comments) => {
     res.send(comments);
   });
 });
 
 router.post("/comment", (req, res) => {
-  const newComment = new Comment({
-    creator_id: req.user._id,
-    creator_name: req.user.name,
-    parent: req.body.parent,
-    content: req.body.content,
-  });
-
-  newComment.save().then((comment) => res.send(comment));
+  
 });
 
 module.exports = router;
