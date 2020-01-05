2b4cd93c52eb6b3bac80b9cf8116895e99db0200
Cory Lynch <cjl2625@gmail.com>
2020-01-05 04:06:00 -0500

    Provide staff CLIENT_ID

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -6,7 +6,7 @@ import { get, post } from "../../utilities";
 import "./NavBar.css";
 
 // This identifies your application to Google's authentication service
-const GOOGLE_CLIENT_ID = "TODO: Fill this out!";
+const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
 
 /**
  * The navigation bar at the top of all pages. Takes no props.
