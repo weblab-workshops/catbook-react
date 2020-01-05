e270cf80c1b83f30cf9e10daba80a98d308b45a0
Cory Lynch <cjl2625@gmail.com>
2020-01-04 22:45:59 -0500

    Import user

--- server/api.js
+++ server/api.js
@@ -12,6 +12,7 @@ const express = require("express");
 // import models so we can interact with the database
 const Story = require("./models/story");
 const Comment = require("./models/comment");
+const Comment = require("./models/user");
 const auth = require("./auth");
 
 // api endpoints: all these paths will be prefixed with "/api/"
