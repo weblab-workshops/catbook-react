c4c2988197c1ef48413b3c132b36c8d9b85f1307
Cory Lynch <cjl2625@gmail.com>
2020-01-04 21:55:47 -0500

    Change profile route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -29,7 +29,7 @@ class App extends Component {
         <div className="App-container">
           <Router>
             <Feed path="/" />
-            <Profile path="/profile/:userId" />
+            <Profile path="/profile" />
             <NotFound default />
           </Router>
         </div>
