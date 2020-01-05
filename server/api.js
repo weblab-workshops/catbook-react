882c496e0e42393ed7802a0aab068e27e7113ff3
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-01-05 18:02:08 -0500

    Restored 404 route

--- server/api.js
+++ server/api.js
@@ -33,3 +33,12 @@ router.get("/comment", (req, res) => {
 router.post("/comment", (req, res) => {
   
 });
+
+// anything else falls to this "not found" case
+router.all("*", (req, res) => {
+  console.log(`API route not found: ${req.method} ${req.url}`);
+  res.status(404).send({ msg: "API route not found" });
+});
+
+
+module.exports = router;
