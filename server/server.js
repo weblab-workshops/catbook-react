72ef46f024227397f56151d0497da3e1c03de682
Kye Burchard <kyeb@mit.edu>
2020-01-08 18:01:11 -0500

    add file extension to require for api

--- server/server.js
+++ server/server.js
@@ -22,13 +22,13 @@ validator.checkSetup();
 const express = require("express"); // backend framework for our node server.
 const path = require("path"); // provide utilities for working with file and directory paths
 
-const api = require("./api");
+const api = require("./api.js");
 
 // create a new express server
 const app = express();
 app.use(validator.checkRoutes);
 
-// connect user-defined routes
+// connect API routes from api.js
 app.use("/api", api);
 
 // load the compiled react files, which will serve /index.html and /bundle.js
