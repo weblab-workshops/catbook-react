8fdffc0c1479785ac9c118b7d5e13eabbb97f66a
Kye Burchard <kyeb@mit.edu>
2020-01-08 15:37:24 -0500

    Remove urlencoded

--- server/server.js
+++ server/server.js
@@ -30,7 +30,6 @@ app.use(validator.checkRoutes);
 
 // automatically parse the body of requests
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 // connect user-defined routes
 app.use("/api", api);
