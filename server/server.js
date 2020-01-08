79919d86a7aa21f42f919a47117e808398831b49
Aaron Sipser <ajsipser@gmail.com>
2020-01-08 14:06:14 -0500

    workshop starter

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@
 | This file defines how your server starts up. Think of it as the main() of your server.
 | At a high level, this file does the following things:
 | - Connect to the database (TODO: WORKSHOP 5)
-| - Sets up server middleware (i.e. addons that enable things like json parsing)
-| - Hooks up all the backend routes specified in api.js
-| - Sets up error handling in case something goes wrong when handling a request
+| - Sets up server middleware (i.e. addons that enable things like json parsing) (TODO: WORKSHOP 3)
+| - Hooks up all the backend routes specified in api.js (TODO: WORKSHOP 4)
+| - Sets up error handling in case something goes wrong when handling a request (TODO: WORKSHOP 3)
 | - Actually starts the webserver
 */
 
@@ -17,35 +17,6 @@ const express = require("express"); // backend framework for our node server.
 
 // create a new express server
 const app = express();
-
-// allow us to parse POST request data using middleware
-app.use(express.json());
-
-//create basic GET endpoint at /api/test
-app.get("/api/test", (req, res) => {
-  res.send({ message: "test API GET endpoint!" });
-});
-
-//create basic POST endpoint at /api/post-here
-app.post("/api/post-here", (req, res) => {
-  res.send({ message: `You sent over ${req.body} data to me!` });
-});
-
-// any server errors cause this function to run
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  if (status === 500) {
-    // 500 means Internal Server Error
-    console.log("The server errored when processing a request!");
-    console.log(err);
-  }
-
-  res.status(status);
-  res.send({
-    status: status,
-    message: err.message,
-  });
-});
 
 // hardcode port to 3000 for now
 const port = 3000;
