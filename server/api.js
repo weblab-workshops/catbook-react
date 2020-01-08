0026ad130132d21dcf7d5f4055344e35f7cac0a8
Kye Burchard <kyeb@mit.edu>
2020-01-08 17:22:16 -0500

    remove data object from starter branch

--- server/api.js
+++ server/api.js
@@ -8,26 +8,3 @@
 */
 
 const express = require("express");
-
-// we haven't set up user login yet, so just
-// use a hardcoded name for now
-// TODO change to a unique name for workshop
-const MY_NAME = "Anonymous User";
-
-let data = {
-  stories: [
-    {
-      _id: 0,
-      creator_name: "Shannen Wu",
-      content: "I love corgis!",
-    },
-  ],
-  comments: [
-    {
-      _id: 0,
-      creator_name: "Jessica Tang",
-      parent: 0,
-      content: "Wow! Me too!",
-    },
-  ],
-};
