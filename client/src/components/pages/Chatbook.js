81f81a230d9724c531c6febabb7619078135e1f5
Alex Chen <achen19@mit.edu>
2020-01-11 00:26:18 -0500

    step4

--- client/src/components/pages/Chatbook.js
+++ client/src/components/pages/Chatbook.js
@@ -12,6 +12,23 @@ const ALL_CHAT = {
   name: "ALL CHAT",
 };
 
+const TEST_MESSAGES = [
+  {
+    sender: {
+      _id: 0,
+      name: "Alex",
+    },
+    content: "i love aaron sippy cup",
+  },
+  {
+    sender: {
+      _id: 0,
+      name: "Nik",
+    },
+    content: "i spend too much time on piazza",
+  },
+];
+
 class Chatbook extends Component {
   /**
    * @typedef UserObject
@@ -34,26 +51,20 @@ class Chatbook extends Component {
     this.state = {
       activeChat: {
         recipient: ALL_CHAT,
-        messages: [],
+        messages: TEST_MESSAGES,
       },
     };
   }
 
   loadMessageHistory(recipient) {
-    get("/api/chat", { recipient_id: recipient._id }).then((messages) => {
-      this.setState({
-        activeChat: {
-          recipient: recipient,
-          messages: messages,
-        },
-      });
-    });
+    // This function loads the messages by getting them from the server
+    // TODO (step 4): get /api/chat and set the activeChat
   }
 
   componentDidMount() {
     document.title = "Chatbook";
 
-    this.loadMessageHistory(ALL_CHAT);
+    // TODO (step 4): call this.loadMessageHistory()
 
     // TODO (step 6): add socket.on for when received message
   }
