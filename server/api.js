4c5e5d6187259f71ac172c0a0b8a9df8ba7d5ef0
Alex Chen <achen19@mit.edu>
2020-01-11 23:24:49 -0500

    remove init socket

--- server/api.js
+++ server/api.js
@@ -74,7 +74,7 @@ router.get("/user", (req, res) => {
 
 router.post("/initsocket", (req, res) => {
   // do nothing if user not logged in
-  if (req.user) socket.addUser(req.user, req.body.socketid);
+  // TODO (step 0): addUser when init socket
   res.send({});
 });
 
