57360ddae28a578ce5ecf3f8861af10f20f84fdc
Alex Chen <achen19@mit.edu>
2020-01-12 01:18:59 -0500

    fix server socket

--- server/server-socket.js
+++ server/server-socket.js
@@ -1,29 +1,29 @@
 let io;
 
-const userToSocketMap = {}; // maps user ID to socket ID
-const socketToUserMap = {}; // maps socket ID to userID
+const userToSocketMap = {}; // maps user ID to socket object
+const socketToUserMap = {}; // maps socket ID to user object
 
 const getAllConnectedUsers = () => Object.values(socketToUserMap);
 const getSocketFromUserID = (userid) => userToSocketMap[userid];
 const getUserFromSocketID = (socketid) => socketToUserMap[socketid];
 const getSocketFromSocketID = (socketid) => io.sockets.connected[socketid];
 
-const addUser = (user, socketid) => {
+const addUser = (user, socket) => {
   const oldSocket = userToSocketMap[user._id];
-  if (oldSocket && oldSocket != socketid) {
+  if (oldSocket && oldSocket.id !== socket.id) {
     // there was an old tab open for this user, force it to disconnect
-    io.to(oldSocket).emit("forceDisconnect");
-    delete socketToUserMap[oldSocket];
+    oldSocket.disconnect();
+    delete socketToUserMap[oldSocket.id];
   }
 
-  userToSocketMap[user._id] = socketid;
-  socketToUserMap[socketid] = user;
+  userToSocketMap[user._id] = socket;
+  socketToUserMap[socket.id] = user;
   io.emit("activeUsers", { activeUsers: getAllConnectedUsers() });
 };
 
-const removeUser = (user, socketid) => {
-  if (user) delete userToSocketMap[user.id];
-  delete socketToUserMap[socketid];
+const removeUser = (user, socket) => {
+  if (user) delete userToSocketMap[user._id];
+  delete socketToUserMap[socket.id];
   io.emit("activeUsers", { activeUsers: getAllConnectedUsers() });
 };
 
@@ -35,7 +35,7 @@ module.exports = {
       console.log(`socket has connected ${socket.id}`);
       socket.on("disconnect", (reason) => {
         const user = getUserFromSocketID(socket.id);
-        removeUser(user, socket.id);
+        removeUser(user, socket);
       });
     });
   },
