8c8e44de0112abe93a6cd453e5879091bac081e2
Alex Chen <achen19@mit.edu>
2020-01-11 23:59:21 -0500

    remove step8s

--- client/src/components/pages/Chatbook.js
+++ client/src/components/pages/Chatbook.js
@@ -84,7 +84,6 @@ class Chatbook extends Component {
 
     return (
       <>
-        {/* TODO (step 8) */}
         <div className="u-flex u-relative Chatbook-container">
           <div className="Chatbook-userList">
             <ChatList
