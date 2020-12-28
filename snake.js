7b403aa20a65c84c207cb2359285004faed5b46a
Daniel Sun <dansun@mit.edu>
2020-12-27 22:47:40 -0800

    fix movement lag

--- snake.js
+++ snake.js
@@ -12,9 +12,9 @@ updateSnake = () => {
     for (let i = snakeBody.length - 2; i >= 0; i--) {
         snakeBody[i + 1] = { x: snakeBody[i].x, y: snakeBody[i].y }
     }
-
-    snakeBody[0].y += inputDirection.y
-    snakeBody[0].x += inputDirection.x
+    let newDirection = getInputDirection()
+    snakeBody[0].y += newDirection.y
+    snakeBody[0].x += newDirection.x
 
     console.log(snakeBody[0])
 }
