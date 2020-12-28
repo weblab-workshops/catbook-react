7b8a42feded7ee2e5403d00f98674dd8f2ac4660
Nikhil Singhal <niks@mit.edu>
2020-12-27 23:12:54 -0800

    Remove higher step TODOs

--- client/src/components/pages/Chatbook.js
+++ client/src/components/pages/Chatbook.js
@@ -26,22 +26,13 @@ class Chatbook extends Component {
 
   constructor(props) {
     super(props);
-    // TODO (step 1): add state activeChat , an object with two fields:
-    // recipient, and messages!
-    // initialize recipient to ALL_CHAT
   }
 
   loadMessageHistory(recipient) {
-    // This function loads the messages by getting them from the server
-    // TODO (step 4): get /api/chat and set the activeChat
   }
 
   componentDidMount() {
     document.title = "Chatbook";
-
-    // TODO (step 4): call this.loadMessageHistory()
-
-    // TODO (step 5): add socket.on for when received message
   }
 
   render() {
