62ae93da94fe3e65a1bea63d459104411467381e
Daniel Sun <dansun@mit.edu>
2020-12-27 20:39:15 -0800

    moving snake

--- input.js
+++ input.js
@@ -1,1 +1,13 @@
-// TODO: fill me in!
+let inputDirection = { x: 0, y: 1 }
+
+window.addEventListener('keydown', event => {
+    if (event.key === "ArrowUp" && inputDirection.x !== 0) {
+        inputDirection = { x: 0, y: 1 }
+    } else if (event.key === "ArrowDown" && inputDirection.x !== 0) {
+        inputDirection = { x: 0, y: -1 }
+    } else if (event.key === "ArrowRight" && inputDirection.y !== 0) {
+        inputDirection = { x: 1, y: 0 }
+    } else if (event.key === "ArrowLeft" && inputDirection.y !== 0) {
+        inputDirection = { x: -1, y: 0 }
+    }
+})
