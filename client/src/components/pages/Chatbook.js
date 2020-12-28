54745fb673c7b409e28c859d7bebae3767ac99bb
Nikhil Singhal <niks@mit.edu>
2020-12-28 03:26:46 -0800

    Update TODO 4 to 6.1 and remove higher todos

--- client/src/components/pages/Chatbook.js
+++ client/src/components/pages/Chatbook.js
@@ -68,7 +68,6 @@ class Chatbook extends Component {
     });
 
     socket.on("message", (data) => {
-      // TODO (step 7)
       this.setState((prevstate) => ({
         activeChat: {
           recipient: prevstate.activeChat.recipient,
@@ -77,14 +76,12 @@ class Chatbook extends Component {
       }));
     });
 
-    // TODO (step 4): add a socket on that listens for the "activeUsers" event. Once we
+    // TODO (step 6.1): add a socket on that listens for the "activeUsers" event. Once we
     // hear the event, update the state with the new active user who just joined
     // similar to get("/api/activeUsers")!
   }
 
   setActiveUser = (user) => {
-    // TODO (step 5): first load the message history for this user, then set the
-    // state "activeChat" to the new recipient (user) and empty array forr messages
     console.log(`setting active user to ${user.name}`);
   };
 
