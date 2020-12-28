1895cff9dd0b41eaddf7804219edcc81a67e517f
Nico Salinas <salinas.nsalinas@gmail.com>
2020-12-28 14:06:15 -0800

    Updated for step 3

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@
 
 // import libraries needed for the webserver to work!
 const express = require("express"); // backend framework for our node server.
+const path = require("path"); // provide utilities for working with file and directory paths
 
 // create a new express server
 const app = express();
@@ -36,6 +37,15 @@ app.use((err, req, res, next) => {
     status: status,
     message: err.message,
   });
+});
+
+// Load the compiled react files, which will serve /index.html and /bundle.js
+const reactPath = path.resolve(__dirname, "..", "client", "dist");
+app.use(express.static(reactPath));
+
+// for all other routes, render index.html and let the react router handle it
+app.get("*", (req, res) => {
+  res.sendFile(path.join(reactPath, "index.html"));
 });
 
 // hardcode port to 3000 for now
