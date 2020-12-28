e8612dba8c97cee66df1f82bb05bf9546aba4218
Nico Salinas <salinas.nsalinas@gmail.com>
2020-12-28 14:08:27 -0800

    Updated step3 for 2021

--- server/server.js
+++ server/server.js
@@ -23,6 +23,15 @@ app.get("/api/test", (req, res) => {
   res.send({ message: "Wow I made my first API!" });
 });
 
+// Load the compiled react files, which will serve /index.html and /bundle.js
+const reactPath = path.resolve(__dirname, "..", "client", "dist");
+app.use(express.static(reactPath));
+
+// for all other routes, render index.html and let the react router handle it
+app.get("*", (req, res) => {
+  res.sendFile(path.join(reactPath, "index.html"));
+});
+
 // any server errors cause this function to run
 app.use((err, req, res, next) => {
   const status = err.status || 500;
@@ -37,15 +46,6 @@ app.use((err, req, res, next) => {
     status: status,
     message: err.message,
   });
-});
-
-// Load the compiled react files, which will serve /index.html and /bundle.js
-const reactPath = path.resolve(__dirname, "..", "client", "dist");
-app.use(express.static(reactPath));
-
-// for all other routes, render index.html and let the react router handle it
-app.get("*", (req, res) => {
-  res.sendFile(path.join(reactPath, "index.html"));
 });
 
 // hardcode port to 3000 for now
