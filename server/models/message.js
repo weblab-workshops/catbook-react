837baddb10fd547404ec6acbea96bca75311df8c
Akshaj Kadaveru <akshajk@mit.edu>
2020-12-28 17:05:49 -0500

    changed comment

--- server/models/message.js
+++ server/models/message.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 //define a message schema for the database
 const MessageSchema = new mongoose.Schema({
-    // TODO (step 3.1): populate schema
+    // TODO (step 3.1): Write the schema for a message
 });
 
 // compile model from schema
