c76a5d1b9890db05e2cd45040c5a779c81f13600
Daniel Sun <dansun@mit.edu>
2020-12-28 15:17:27 -0800

    starter code

--- game.js
+++ game.js
@@ -1,14 +1,5 @@
-let lastRenderTime = 0;
-
-const gameBoard = document.getElementById('game-board')
 main = (currentTime) => {
     window.requestAnimationFrame(main);
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
-
-    console.log('Render');
-    lastRenderTime = currentTime;
-
     update();
     draw();
 }
@@ -16,17 +7,10 @@ main = (currentTime) => {
 window.requestAnimationFrame(main);
 
 update = () => {
-    updateFood();
-    updateSnake();
-    checkDeath();
 }
 
 draw = () => {
-    gameBoard.innerHTML = ''
-    drawSnake(gameBoard);
-    drawFood(gameBoard);
 }
 
 checkDeath = () => {
-    //TODO:fill me in
 }
--- grid.js
+++
@@ -1,8 +0,0 @@
-const GRID_SIZE = 21;
-
-randomGridPosition = () => {
-    return ({
-        x: Math.floor(Math.random() * GRID_SIZE) + 1,
-        y: Math.floor(Math.random() * GRID_SIZE) + 1
-    })
-}
