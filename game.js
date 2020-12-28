1ee53446c50c32aafbc61b849e014dc34e434639
Daniel Sun <dansun@mit.edu>
2020-12-28 15:26:14 -0800

    starter should be good now

--- game.js
+++ game.js
@@ -1,14 +1,5 @@
-let lastRenderTime = 0;
-let SNAKE_SPEED = 5;
-
 main = (currentTime) => {
     window.requestAnimationFrame(main);
-    let secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) {
-        return;
-    }
-
-    lastRenderTime = currentTime;
     update();
     draw();
 }
@@ -16,7 +7,6 @@ main = (currentTime) => {
 window.requestAnimationFrame(main);
 
 update = () => {
-    console.log("Render");
 }
 
 draw = () => {
