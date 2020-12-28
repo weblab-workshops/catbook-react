5a3fbd400778dce716286a0049f4cdd651ca4e42
Daniel Sun <dansun@mit.edu>
2020-12-28 15:22:47 -0800

    snake speed

--- game.js
+++ game.js
@@ -1,5 +1,14 @@
+let lastRenderTime = 0;
+let SNAKE_SPEED = 5;
+
 main = (currentTime) => {
     window.requestAnimationFrame(main);
+    let secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) {
+        return;
+    }
+
+    lastRenderTime = currentTime;
     update();
     draw();
 }
@@ -7,10 +16,8 @@ main = (currentTime) => {
 window.requestAnimationFrame(main);
 
 update = () => {
+    console.log("Render");
 }
 
 draw = () => {
-}
-
-checkDeath = () => {
 }
