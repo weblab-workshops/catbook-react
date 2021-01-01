af092187f2547e409f4ff46d987c6117af4ba74c
Kye Burchard <kyeb@mit.edu>
2020-12-31 21:46:56 -0500

    attempt at build script + gauth

--- server/auth.js
+++ server/auth.js
@@ -3,7 +3,7 @@ const User = require("./models/user");
 const socketManager = require("./server-socket");
 
 // create a new OAuth client used to verify google sign-in
-const CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const CLIENT_ID = "429745605307-n9f9rgs2d1l7mimufms3s32m8cr4j2ju.apps.googleusercontent.com";
 const client = new OAuth2Client(CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
