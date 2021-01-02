bf2bad1af79969e7da501df5c19e8be6f537754a
Noah Raby <noahr8aby@gmail.com>
2021-01-02 01:56:59 -0600

    Slides ready

--- server/api.js
+++ server/api.js
@@ -7,4 +7,74 @@
 |
 */
 
+// without a system for users, we'll have to hardcode our user name
+const MY_NAME = "Hackerman";
+
+const data = {
+  stories: [
+    {
+      _id: 0,
+      creator_name: "Shannen Wu",
+      content: "I love corgis!"
+    }
+  ],
+  comments: [
+    {
+      _id: 0,
+      creator_name: "Jessica Tang",
+      parent: 0,
+      content: "Wow! Me Too!",
+    }
+  ],
+};
+
 const express = require("express");
+
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+  res.send({ message: "Wow I made my first API! In its own file!" });
+});
+
+router.get("/stories", (req, res) => {
+  // send back all of the stories!
+  res.send(data.stories);
+});
+
+router.get("/comment", (req, res) => {
+  const filteredComments = data.comments.filter(
+    (comment) => comment.parent == req.query.parent);
+  res.send(filteredComments)
+});
+
+router.post("/story", (req, res) => {
+  const newStory = {
+    _id: data.stories.length,
+    creator_name: MY_NAME,
+    content: req.body.content,
+  };
+  
+  data.stories.push(newStory);
+  res.send(newStory);
+});
+
+router.post("/comment", (req, res) => {
+  const newComment = {
+    _id: data.comments.length,
+    creator_name: MY_NAME,
+    parent: req.query.parent,
+    content: req.body.content,
+  };
+  
+  data.comments.push(newComment);
+  res.send(newComment);
+});
+
+// similar to our other catch all route in server.js,
+// let's add a backup route for bad /api routes
+router.all("*", (req, res) => {
+  console.log(`API Route not found: ${req.method} ${req.url}`);
+  res.status(404).send({ message: "API Route not found" });
+});
+
+module.exports = router;
