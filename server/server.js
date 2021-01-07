f569f4b063903126e3af16ab64ce4168cab768de
Nikhil Singhal <niks@mit.edu>
2021-01-07 02:41:42 -0500

    Use || not |

--- server/server.js
+++ server/server.js
@@ -77,7 +77,7 @@ app.use((err, req, res, next) => {
 });
 
 // hardcode port to 3000 for now
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 const server = http.Server(app);
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
