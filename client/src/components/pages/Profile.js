807d9b3518a340d5f9c708b06a6a825e2a6fc7e3
Kye Burchard <kyeb@mit.edu>
2021-01-11 23:54:57 -0500

    functionalize profile so profile updates right

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -6,70 +6,60 @@ import { get } from "../../utilities";
 import "../../utilities.css";
 import "./Profile.css";
 
-class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: undefined,
-      catHappiness: 0,
-    };
-  }
+const { useState, useEffect } = React;
 
-  componentDidMount() {
+const Profile = (props) => {
+  const [user, setUser] = useState(undefined);
+  const [catHappiness, setCatHappiness] = useState(0);
+
+  useEffect(() => {
     document.title = "Profile Page";
-    get(`/api/user`, { userid: this.props.userId }).then((user) => this.setState({ user: user }));
-  }
+  }, []);
 
-  incrementCatHappiness = () => {
-    this.setState({
-      catHappiness: this.state.catHappiness + 1,
-    });
+  useEffect(() => {
+    get(`/api/user`, { userid: props.userId }).then((newUser) => setUser(newUser));
+  }, [props.userId]);
+
+  const incrementCatHappiness = () => {
+    setCatHappiness(catHappiness + 1);
   };
 
-  updateUser = (user) => {
-    this.setState({ user: user });
-  };
-
-  render() {
-    if (!this.state.user) {
-      return <div> Loading! </div>;
-    }
-    return (
-      <>
-        <div
-          className="Profile-avatarContainer"
-          onClick={() => {
-            this.incrementCatHappiness();
-          }}
-        >
-          <div className="Profile-avatar" />
-        </div>
-        <h1 className="Profile-name u-textCenter">{this.state.user.name}</h1>
-        <hr className="Profile-line" />
-        <div className="u-flex">
-          <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">About Me</h4>
-            <div
-              id="profile-description"
-              style={{ paddingBottom: "100px" }}
-              dangerouslySetInnerHTML={{ __html: this.state.user.bio }}
-            />
-            {this.state.user._id === this.props.self && (
-              <NewBio self={this.props.self} updateUser={this.updateUser} />
-            )}
-          </div>
-          <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">Cat Happiness</h4>
-            <CatHappiness catHappiness={this.state.catHappiness} />
-          </div>
-          <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
-            <div id="favorite-cat">corgi</div>
-          </div>
-        </div>
-      </>
-    );
+  if (!user) {
+    return <div> Loading! </div>;
   }
-}
+  return (
+    <>
+      <div
+        className="Profile-avatarContainer"
+        onClick={() => {
+          incrementCatHappiness();
+        }}
+      >
+        <div className="Profile-avatar" />
+      </div>
+      <h1 className="Profile-name u-textCenter">{user.name}</h1>
+      <hr className="Profile-line" />
+      <div className="u-flex">
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">About Me</h4>
+          <div
+            id="profile-description"
+            style={{ paddingBottom: "100px" }}
+            dangerouslySetInnerHTML={{ __html: user.bio }}
+          />
+          {user._id === props.self && <NewBio self={props.self} updateUser={setUser} />}
+        </div>
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">Cat Happiness</h4>
+          <CatHappiness catHappiness={catHappiness} />
+        </div>
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
+          <div id="favorite-cat">corgi</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Profile;
