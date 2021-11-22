705b63bd0114667fcc94b29cd5b857b94ffe0130
AkshajK <akshajk@mit.edu>
2021-11-22 00:41:42 -0500

    Update for node 16

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
   plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
     historyApiFallback: true,
-    contentBase: "./client/dist",
+    static: "./client/dist",
     hot: true,
     proxy: {
       "/api": "http://localhost:3000",
