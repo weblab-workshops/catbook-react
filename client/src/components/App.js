30bb069bd54789fa7154dc6f0b8fa0d37cbdbfd6
AkshajK <akshajk@mit.edu>
2021-11-22 00:53:05 -0500

    Convert w2-step1 to functional

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import NavBar from "./modules/NavBar.js";
 import Profile from "./pages/Profile.js";
 
@@ -9,21 +9,19 @@ import "./App.css";
 /**
  * Define the "App" component as a class.
  */
-class App extends Component {
+const App = () => {
   // required method: whatever is returned defines what
   // shows up on screen
-  render() {
-    return (
-      // <> is like a <div>, but won't show
-      // up in the DOM tree
-      <>
-        <NavBar />
-        <div className="App-container">
-          <Profile />
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    // <> is like a <div>, but won't show
+    // up in the DOM tree
+    <>
+      <NavBar />
+      <div className="App-container">
+        <Profile />
+      </div>
+    </>
+  );
+};
 
 export default App;
--- client/src/components/modules/CatHappiness.js
+++ client/src/components/modules/CatHappiness.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./CatHappiness.css";
 
 /**
@@ -7,20 +7,14 @@ import "./CatHappiness.css";
  * Proptypes
  * @param {int} catHappiness is how happy your cat is
  */
-class CatHappiness extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="CatHappiness-container">
-        <div className="CatHappiness-story">
-          <p className="CatHappiness-storyContent">{/* TODO: insert happiness counter here!*/}</p>
-        </div>
+const CatHappiness = () => {
+  return (
+    <div className="CatHappiness-container">
+      <div className="CatHappiness-story">
+        <p className="CatHappiness-storyContent">{/* TODO: insert happiness counter here!*/}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default CatHappiness;
