c776cb848664484214263353b7992d8dfdfe674c
AkshajK <akshajk@mit.edu>
2021-11-26 01:49:36 -0500

    app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import NavBar from "./modules/NavBar.js";
 import Profile from "./pages/Profile.js";
 // TODO (step0): import Feed
@@ -9,34 +9,21 @@ import "../utilities.css";
 import "./App.css";
 
 /**
- * Define the "App" component as a class.
+ * Define the "App" component as a function.
  */
-class App extends Component {
-  // Lifecycle method: makes props available
-  constructor(props) {
-    super(props);
-  }
-
-  // Lifecycle method: called when component
-  // "mounts", i.e. when it shows up on screen
-  componentDidMount() {}
-
-  // Required lifecycle method: defines what
-  // shows up on screen
-  render() {
-    return (
-      // <> is like a <div>, but won't show
-      // up in the DOM tree
-      <>
-        <NavBar />
-        <div className="App-container">
-          <Profile />
-          {/* TODO (step0): render Feed instead of Profile */}
-          {/* TODO (step5): use Router to route between pages */}
-        </div>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    // <> is like a <div>, but won't show
+    // up in the DOM tree
+    <>
+      <NavBar />
+      <div className="App-container">
+        <Profile />
+        {/* TODO (step0): render Feed instead of Profile */}
+        {/* TODO (step5): use Router to route between pages */}
+      </div>
+    </>
+  );
+};
 
 export default App;
