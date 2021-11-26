d63b0b9b528f7eff04339a454eef7d7605b15044
AkshajK <akshajk@mit.edu>
2021-11-26 01:51:37 -0500

    Converrt feed

--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -1,28 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { get } from "../../utilities";
 // TODO (step2): import SingleStory
 // TODO (step4): import NewStory
 // TODO (step6): remove SingleStory import, import Card
 
-class Feed extends Component {
-  constructor(props) {
-    super(props);
-    // TODO (step1): define state to hold stories
-  }
+const Feed = () => {
+  // TODO (step1): define state to hold stories
 
-  componentDidMount() {
+  useEffect(() => {
     // TODO (step1): implement a GET call to retrieve stories,
     // and assign it to state
-  }
+  }, []);
 
-  render() {
-    return <div>This is the feed!</div>;
-    // TODO (step1): render the raw stories data from state
-    // TODO (step2): render a SingleStory with hardcoded props
-    // TODO (step3): map the state to SingleStory components
-    // TODO (step4): add in the NewStory component
-    // TODO (step6): use Card instead of SingleStory
-  }
-}
+  return <div>This is the feed!</div>;
+  // TODO (step1): render the raw stories data from state
+  // TODO (step2): render a SingleStory with hardcoded props
+  // TODO (step3): map the state to SingleStory components
+  // TODO (step4): add in the NewStory component
+  // TODO (step6): use Card instead of SingleStory
+};
 
 export default Feed;
