6951711fd1f4e18f357358eb8dae87ff4899e305
AkshajK <akshajk@mit.edu>
2021-11-28 17:58:24 -0500

    Convert w4 to functional

--- client/src/components/modules/SingleStory.js
+++ client/src/components/modules/SingleStory.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import { Link } from "@reach/router";
+import React from "react";
 
 /**
  * Story is a component that renders creator and content of a story
@@ -9,19 +8,13 @@ import { Link } from "@reach/router";
  * @param {string} creator_name
  * @param {string} content of the story
  */
-class SingleStory extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="Card-story">
-        <span className="u-bold">{this.props.creator_name}</span>
-        <p className="Card-storyContent">{this.props.content}</p>
-      </div>
-    );
-  }
-}
+const SingleStory = (props) => {
+  return (
+    <div className="Card-story">
+      <span className="u-bold">{props.creator_name}</span>
+      <p className="Card-storyContent">{props.content}</p>
+    </div>
+  );
+};
 
 export default SingleStory;
--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,59 +1,50 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CatHappiness from "../modules/CatHappiness.js";
 
 import "../../utilities.css";
 import "./Profile.css";
 
-class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      catHappiness: 0,
-    };
-  }
+const Profile = () => {
+  const [catHappiness, setCatHappiness] = useState(0);
 
-  componentDidMount() {
+  useEffect(() => {
     document.title = "Profile Page";
-  }
+  }, []);
 
-  incrementCatHappiness = () => {
-    this.setState({
-      catHappiness: this.state.catHappiness + 1,
-    });
+  const incrementCatHappiness = () => {
+    setCatHappiness(catHappiness + 1);
   };
 
-  render() {
-    return (
-      <>
-        <div
-          className="Profile-avatarContainer"
-          onClick={() => {
-            this.incrementCatHappiness();
-          }}
-        >
-          <div className="Profile-avatar" />
-        </div>
-        <h1 className="Profile-name u-textCenter">Shannen Wu</h1>
-        <hr className="Profile-line" />
-        <div className="u-flex">
-          <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">About Me</h4>
-            <div id="profile-description">
-              I am really allergic to cats i don't know why i have a catbook
-            </div>
-          </div>
-          <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">Cat Happiness</h4>
-            <CatHappiness catHappiness={this.state.catHappiness} />
-          </div>
-          <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
-            <div id="favorite-cat">corgi</div>
+  return (
+    <>
+      <div
+        className="Profile-avatarContainer"
+        onClick={() => {
+          incrementCatHappiness();
+        }}
+      >
+        <div className="Profile-avatar" />
+      </div>
+      <h1 className="Profile-name u-textCenter">Shannen Wu</h1>
+      <hr className="Profile-line" />
+      <div className="u-flex">
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">About Me</h4>
+          <div id="profile-description">
+            I am really allergic to cats i don't know why i have a catbook
           </div>
         </div>
-      </>
-    );
-  }
-}
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">Cat Happiness</h4>
+          <CatHappiness catHappiness={catHappiness} />
+        </div>
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
+          <div id="favorite-cat">corgi</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Profile;
