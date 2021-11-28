3e81111bb1edec850c0aafb5cd28747eb5619702
AkshajK <akshajk@mit.edu>
2021-11-28 17:58:24 -0500

    Convert w4 to functional

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import NavBar from "./modules/NavBar.js";
 import { Router } from "@reach/router";
 import Feed from "./pages/Feed.js";
@@ -10,32 +10,26 @@ import "../utilities.css";
 import "./App.css";
 
 /**
- * Define the "App" component as a class.
+ * Define the "App" component as a function.
  */
-class App extends Component {
-  // makes props available in this component
-  constructor(props) {
-    super(props);
-  }
-
+const App = () => {
   // required method: whatever is returned defines what
   // shows up on screen
-  render() {
-    return (
-      // <> is like a <div>, but won't show
-      // up in the DOM tree
-      <>
-        <NavBar />
-        <div className="App-container">
-          <Router>
-            <Feed path="/" />
-            <Profile path="/profile/" />
-            <NotFound default />
-          </Router>
-        </div>
-      </>
-    );
-  }
-}
+
+  return (
+    // <> is like a <div>, but won't show
+    // up in the DOM tree
+    <>
+      <NavBar />
+      <div className="App-container">
+        <Router>
+          <Feed path="/" />
+          <Profile path="/profile/" />
+          <NotFound default />
+        </Router>
+      </div>
+    </>
+  );
+};
 
 export default App;
