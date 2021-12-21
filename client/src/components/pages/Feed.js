b0fc6517fbee3747aaee159a01c70ab7626f5a2b
Helen Lu <helenlu2001@gmail.com>
2021-12-21 15:01:30 -0500

    converted step 2 into functional components

--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -1,33 +1,27 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { get } from "../../utilities";
 import SingleStory from "../modules/SingleStory.js";
 // TODO (step4): import NewStory
 // TODO (step6): remove SingleStory import, import Card
 
-class Feed extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stories: [],
-    };
-  }
+const Feed = () => {
+  // TODO (step1): define state to hold stories
+  const [stories, setStories] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     get("/api/stories").then((storyObjs) => {
-      this.setState({ stories: storyObjs });
+      setStories(storyObjs);
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <SingleStory creator_name="Matt" content="test" />
-      </div>
-    );
-    // TODO (step3): map the state to SingleStory components
-    // TODO (step4): add in the NewStory component
-    // TODO (step6): use Card instead of SingleStory
-  }
-}
+  return (
+    <div>
+      <SingleStory creator_name="Matt" content="test" />
+    </div>
+  );
+  // TODO (step3): map the state to SingleStory components
+  // TODO (step4): add in the NewStory component
+  // TODO (step6): use Card instead of SingleStory
+};
 
 export default Feed;
