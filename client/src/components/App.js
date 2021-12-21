612b0f48a7429f7e03f9e9bbfe8c03fa651eda9a
Helen Lu <helenlu2001@gmail.com>
2021-12-21 14:18:54 -0500

    forgot to remove component from App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import NavBar from "./modules/NavBar.js";
 import Profile from "./pages/Profile.js";
 import Feed from "./pages/Feed.js";
