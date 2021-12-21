2a93c719cd8f78e73bed3adba45fc88ab014de3e
Helen Lu <helenlu2001@gmail.com>
2021-12-21 15:37:29 -0500

    converted step 4 to functional react

--- client/src/components/modules/Card.js
+++ client/src/components/modules/Card.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { get } from "../../utilities";
 // TODO (step6): import SingleStory
 // TODO (step7): import SingleComment
@@ -15,26 +15,21 @@ import "./Card.css";
  * @param {string} creator_name
  * @param {string} content of the story
  */
-class Card extends Component {
-  constructor(props) {
-    super(props);
-    // TODO (step6): define state to hold comments (refer to Feed)
-  }
+const Card = () => {
+  // TODO (step6): define state to hold comments (refer to Feed)
 
-  componentDidMount() {
+  useEffect(() => {
     // TODO (step6): implement a GET call to retrieve comments,
     // and assign it to state
-  }
+  }, []);
 
-  render() {
-    // TODO (step6): render a SingleStory using props,
-    // and render the comments from state (with JSON.stringify)
-    // from state using a map (refer to Feed)
-    // TODO (step7): map comments from state into SingleComment
-    // components (refer to Feed)
-    // TODO (step8): add in the NewComment component (refer to Feed)
-    // TODO (step9): use CommentsBlock
-  }
-}
+  // TODO (step6): render a SingleStory using props,
+  // and render the comments from state (with JSON.stringify)
+  // from state using a map (refer to Feed)
+  // TODO (step7): map comments from state into SingleComment
+  // components (refer to Feed)
+  // TODO (step8): add in the NewComment component (refer to Feed)
+  // TODO (step9): use CommentsBlock
+};
 
 export default Card;
--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -1,41 +1,36 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { get } from "../../utilities";
 import SingleStory from "../modules/SingleStory.js";
 import { NewStory } from "../modules/NewPostInput.js";
 // TODO (step6): remove SingleStory import, import Card
 
-class Feed extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stories: [],
-    };
-  }
+const Feed = () => {
+  // TODO (step1): define state to hold stories
+  const [stories, setStories] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     get("/api/stories").then((storyObjs) => {
-      this.setState({ stories: storyObjs });
+      setStories(storyObjs);
     });
+  }, []);
+
+  let storiesList = null;
+  const hasStories = stories.length !== 0;
+  if (hasStories) {
+    storiesList = stories.map((storyObj) => (
+      <SingleStory creator_name={storyObj.creator_name} content={storyObj.content} />
+    ));
+  } else {
+    storiesList = <div>No stories!</div>;
   }
 
-  render() {
-    let storiesList = null;
-    const hasStories = this.state.stories.length !== 0;
-    if (hasStories) {
-      storiesList = this.state.stories.map((storyObj) => (
-        <SingleStory creator_name={storyObj.creator_name} content={storyObj.content} />
-      ));
-    } else {
-      storiesList = <div>No stories!</div>;
-    }
-    return (
-      <div>
-        <NewStory />
-        {storiesList}
-      </div>
-    );
-    // TODO (step6): use Card instead of SingleStory
-  }
-}
+  return (
+    <div>
+      <NewStory />
+      {storiesList}
+    </div>
+  );
+  // TODO (step6): use Card instead of SingleStory
+};
 
 export default Feed;
