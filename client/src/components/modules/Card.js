c7a3875a48f569987bb9314b98b7a0db122a74f0
Claire Cheng <clairecheng88@gmail.com>
2021-12-21 19:50:58 -0800

    card

--- client/src/components/modules/Card.js
+++ client/src/components/modules/Card.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { get } from "../../utilities";
 import SingleStory from "./SingleStory.js";
 import SingleComment from "./SingleComment.js";
@@ -15,25 +15,19 @@ import "./Card.css";
  * @param {string} creator_name
  * @param {string} content of the story
  */
-class Card extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      comments: [],
-    };
-  }
+const Card = (props) => {
+  const [comments, setComments] = useState([]);
 
-  componentDidMount() {
-    get("/api/comment", { parent: this.props._id }).then((comments) => {
-      this.setState({ comments: comments });
+  useEffect(() => {
+    get("/api/comment", { parent: props._id }).then((commentItems) => {
+      setComments(commentItems);
     });
-  }
+  }, []);
 
-  render() {
-    let commentsList = null;
-    const hasComments = this.state.comments.length !== 0;
+  let commentsList = null;
+    const hasComments = comments.length !== 0;
     if (hasComments) {
-      commentsList = this.state.comments.map((commentObj) => (
+      commentsList = comments.map((commentObj) => (
         <SingleComment
           _id={commentObj._id}
           creator_name={commentObj.creator_name}
@@ -44,19 +38,20 @@ class Card extends Component {
       commentsList = <div>No comments!</div>;
     }
 
-    return (
-      <div className="Card-container">
+  return (
+    <div className="Card-container">
         <SingleStory
-          _id={this.props._id}
-          creator_name={this.props.creator_name}
-          content={this.props.content}
+          _id={props._id}
+          creator_name={props.creator_name}
+          content={props.content}
         />
         {commentsList}
       </div>
-    );
-    // TODO (step8): add in the NewComment component (refer to Feed)
-    // TODO (step9): use CommentsBlock
-  }
-}
+  )
+  // TODO (step7): map comments from state into SingleComment
+  // components (refer to Feed)
+  // TODO (step8): add in the NewComment component (refer to Feed)
+  // TODO (step9): use CommentsBlock
+};
 
 export default Card;
