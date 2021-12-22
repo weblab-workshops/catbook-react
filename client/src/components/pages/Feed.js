c0e8be92b4cc319ef07929e6d41e473eaf506195
Claire Cheng <clairecheng88@gmail.com>
2021-12-21 20:21:21 -0800

    untouched files

--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -1,42 +1,33 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { get } from "../../utilities";
 import Card from "../modules/Card.js";
 import { NewStory } from "../modules/NewPostInput.js";
 
-class Feed extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stories: [],
-    };
-  }
+const Feed = () => {
+  const [stories, setStories] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     get("/api/stories").then((storyObjs) => {
-      this.setState({ stories: storyObjs });
+      setStories(storyObjs);
     });
-  }
+  }, []);
 
-  // TODO (step10): implement addNewStory
-
-  render() {
-    let storiesList = null;
-    const hasStories = this.state.stories.length !== 0;
-    if (hasStories) {
-      storiesList = this.state.stories.map((storyObj) => (
-        <Card _id={storyObj._id} creator_name={storyObj.creator_name} content={storyObj.content} />
+  let storiesList = null;
+  const hasStories = stories.length !== 0;
+  if (hasStories) {
+    storiesList = stories.map((storyObj) => (
+      <Card _id={storyObj._id} creator_name={storyObj.creator_name} content={storyObj.content} />
       ));
-    } else {
-      storiesList = <div>No stories!</div>;
-    }
-    return (
-      <div>
-        {/* TODO (step10): pass addNewStory as prop to NewStory */}
-        <NewStory />
-        {storiesList}
-      </div>
-    );
+  } else {
+    storiesList = <div>No stories!</div>;
   }
-}
+
+  return (
+    <div>
+      <NewStory />
+      {storiesList}
+    </div>
+  );
+};
 
 export default Feed;
