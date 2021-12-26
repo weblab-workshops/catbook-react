3d598bcb9ef4484d5c4b1273eec66b91455aeb64
Albert B Xing <axing@mit.edu>
2021-12-26 18:38:42 -0500

    step 1

--- game.js
+++ game.js
@@ -1,11 +1,14 @@
+const SNAKE_SPEED = 5;
+
 const main = () => {
     update();
     draw();
 }
 
-setInterval(main);
+setInterval(main, 1000/SNAKE_SPEED);
 
 const update = () => {
+    console.log("Updating")
 }
 
 const draw = () => {
