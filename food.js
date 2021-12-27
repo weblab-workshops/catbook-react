77f85b9a43ba0124991aaea97486ff44f385c007
Albert B Xing <axing@mit.edu>
2021-12-26 21:40:05 -0500

    step 3

--- food.js
+++ food.js
@@ -1,1 +1,11 @@
 // TODO: fill me in!
+
+
+// Don't change me!
+const drawFood = (gameBoard) => {
+    const foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food');
+    gameBoard.appendChild(foodElement);
+}
