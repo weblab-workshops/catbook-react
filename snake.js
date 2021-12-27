8a07c9b8ffb52b1dfe03fe46b5d1efd47187c9b0
Albert B Xing <axing@mit.edu>
2021-12-26 20:38:03 -0500

    step 2

--- snake.js
+++ snake.js
@@ -1,11 +1,18 @@
 const SNAKE_SPEED = 5;
 const snakeBody = [
-    {x:11, y:11},
+    {x:11, y:9},
     {x:11, y:10},
-    {x:11, y:9}
+    {x:11, y:11}
 ];
 
 const updateSnake = () => {
+    // remove tail segment
+    snakeBody.pop();
+
+    // add new head segment
+    snakeBody.unshift({...snakeBody[0]});
+    snakeBody[0].x += 0;
+    snakeBody[0].y += 1;
 
 }
 
