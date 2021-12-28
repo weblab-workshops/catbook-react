ac4603339beea137815950ec2509cebc72016f69
Akshaj Kadaveru <akshajk@mit.edu>
2021-12-28 02:27:33 -0500

    reverse feed

--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -13,9 +13,7 @@ const Feed = () => {
     document.title = "News Feed";
     get("/api/stories").then((storyObjs) => {
       let reversedStoryObjs = storyObjs.reverse();
-      reversedStoryObjs.map((storyObj) => {
-        setStories(stories.concat([storyObj]));
-      });
+      setStories(reversedStoryObjs);
     });
   }, []);
 
