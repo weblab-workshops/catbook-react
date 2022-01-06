db8cb2d6c71ed37420c91ba47ce22035148f8ce7
Daniel Sun <dansun@mit.edu>
2022-01-06 15:30:54 -0500

    notfound.js

--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -1,18 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NotFound extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>404 Not Found</h1>
-        <p>The page you requested couldn't be found.</p>
-      </div>
-    );
-  }
-}
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The page you requested couldn't be found.</p>
+    </div>
+  );
+};
 
 export default NotFound;
