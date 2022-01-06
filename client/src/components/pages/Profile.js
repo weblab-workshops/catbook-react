e1b4635f9cb5cc3108eadd9a78de7fd56b342dc6
Daniel Sun <dansun@mit.edu>
2022-01-06 15:29:10 -0500

    profile.js

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,65 +1,55 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CatHappiness from "../modules/CatHappiness.js";
-import { get } from "../../utilities";
+import { get } from "../../utilities"
 
 import "../../utilities.css";
 import "./Profile.css";
 
-class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: undefined,
-      catHappiness: 0,
-    };
-  }
+const Profile = (props) => {
+  const [catHappiness, setCatHappiness] = useState(0);
+  const [user, setUser] = useState();
 
-  componentDidMount() {
+  useEffect(() => {
     document.title = "Profile Page";
-    get(`/api/user`, { userid: this.props.userId }).then((user) => this.setState({ user: user }));
-  }
+    get(`/api/user`, { userid: props.userId }).then((userObj) => setUser(userObj));
+  }, []);
 
-  incrementCatHappiness = () => {
-    this.setState({
-      catHappiness: this.state.catHappiness + 1,
-    });
+  const incrementCatHappiness = () => {
+    setCatHappiness(catHappiness + 1);
   };
 
-  render() {
-    if (!this.state.user) {
-      return <div> Loading! </div>;
-    }
-    return (
-      <>
-        <div
-          className="Profile-avatarContainer"
-          onClick={() => {
-            this.incrementCatHappiness();
-          }}
-        >
-          <div className="Profile-avatar" />
-        </div>
-        <h1 className="Profile-name u-textCenter">{this.state.user.name}</h1>
-        <hr className="Profile-line" />
-        <div className="u-flex">
-          <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">About Me</h4>
-            <div id="profile-description">
-              Extra Challenge: Modify catbook to show a personalized description here!
-            </div>
-          </div>
-          <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">Cat Happiness</h4>
-            <CatHappiness catHappiness={this.state.catHappiness} />
-          </div>
-          <div className="Profile-subContainer u-textCenter">
-            <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
-            <div id="favorite-cat">corgi</div>
-          </div>
-        </div>
-      </>
-    );
+  if (!user) {
+    return (<div> Loading! </div>);
   }
-}
+  return (
+    <>
+      <div
+        className="Profile-avatarContainer"
+        onClick={() => {
+          incrementCatHappiness();
+        }}
+      >
+        <div className="Profile-avatar" />
+      </div>
+      <h1 className="Profile-name u-textCenter">{user.name}</h1>
+      <hr className="Profile-linejj" />
+      <div className="u-flex">
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">About Me</h4>
+          <div id="profile-description">
+            I am really allergic to cats i don't know why i have a catbook
+          </div>
+        </div>
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">Cat Happiness</h4>
+          <CatHappiness catHappiness={catHappiness} />
+        </div>
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
+          <div id="favorite-cat">corgi</div>
+        </div>
+      </div>
+    </>)
+};
 
 export default Profile;
