6256d59ae8c3ac9ff9d62e4319acd43e5c361998
Nikhil Singhal <niks@mit.edu>
2022-01-09 10:14:55 -0500

    Forgot some components

--- client/src/components/modules/Modal.js
+++ client/src/components/modules/Modal.js
@@ -9,22 +9,14 @@ import "./Modal.css";
  * @param {bool} show true if this modal should be displayed
  * @param {string} message the text to be displayed
  */
-class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {}
-
-  render() {
-    return (
-      this.props.show && (
-        <div className="Modal-background u-flex u-flex-justifyCenter u-flex-alignCenter">
-          <div className="Modal-container u-positionRelative">{this.props.message}</div>
-        </div>
-      )
-    );
-  }
+const Modal = (props) => {
+  return (
+    props.show && (
+      <div className="Modal-background u-flex u-flex-justifyCenter u-flex-alignCenter">
+        <div className="Modal-container u-positionRelative">{props.message}</div>
+      </div>
+    )
+  );
 }
 
 export default Modal;
