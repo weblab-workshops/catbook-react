f641c35e1d4c60f2a45481c4c03f346956ea5d1e
Nicholas Tsao <ntt1729@gmail.com>
2023-01-02 15:42:59 -0600

    removed sprites

--- client/src/canvasManager.js
+++ client/src/canvasManager.js
@@ -2,21 +2,6 @@ let canvas;
 
 /** utils */
 
-// load sprites!
-let sprites = {
-  red: null,
-  blue: null,
-  green: null,
-  yellow: null,
-  purple: null,
-  orange: null,
-  silver: null,
-};
-Object.keys(sprites).forEach((key) => {
-  sprites[key] = new Image(400, 400);
-  sprites[key].src = `../player-icons/${key}.png`; // Load sprites from dist folder
-});
-
 // converts a coordinate in a normal X Y plane to canvas coordinates
 const convertCoord = (x, y) => {
   if (!canvas) return;
@@ -34,24 +19,11 @@ const fillCircle = (context, x, y, radius, color) => {
   context.fill();
 };
 
-// draws a sprite instead of a colored circle
-const drawSprite = (context, x, y, radius, color) => {
-  context.save();
-  // Saves current context so we can restore to here once we are done drawing
-  context.beginPath();
-  context.arc(x, y, radius, 0, 2 * Math.PI, false);
-  context.closePath();
-  context.clip(); // Sets circular clipping region for sprite image
-  context.drawImage(sprites[color], x - radius, y - radius, radius * 2, radius * 2);
-  context.restore();
-  // Restores context to last save (before clipping was applied), so we can draw normally again
-};
-
 /** drawing functions */
 
 const drawPlayer = (context, x, y, radius, color) => {
   const { drawX, drawY } = convertCoord(x, y);
-  drawSprite(context, drawX, drawY, radius, color);
+  fillCircle(context, drawX, drawY, radius, color);
 };
 
 const drawCircle = (context, x, y, radius, color) => {
