e8364f85dee55d3c2a785406a2db80bc8f11e1bf
Qiong Zhou Huang <qiongzhouh@gmail.com>
2023-01-10 19:39:15 -0500

    Make w2 deployable on heroku

---
+++ server/server.js
@@ -0,0 +1,55 @@
+/*
+|--------------------------------------------------------------------------
+| server.js -- The core of your server
+|--------------------------------------------------------------------------
+|
+| This file defines how your server starts up. Think of it as the main() of your server.
+| At a high level, this file does the following things:
+| - Connect to the database
+| - Sets up server middleware (i.e. addons that enable things like json parsing, user login)
+| - Hooks up all the backend routes specified in api.js
+| - Fowards frontend routes that should be handled by the React router
+| - Sets up error handling in case something goes wrong when handling a request
+| - Actually starts the webserver
+*/
+
+//import libraries needed for the webserver to work!
+const http = require("http");
+const express = require("express"); // backend framework for our node server.
+const path = require("path"); // provide utilities for working with file and directory paths
+
+// // create a new express server
+const app = express();
+
+// load the compiled react files, which will serve /index.html and /bundle.js
+const reactPath = path.resolve(__dirname, "..", "client", "dist");
+app.use(express.static(reactPath));
+
+// for all other routes, render index.html and let react router handle it
+app.get("*", (req, res) => {
+  res.sendFile(path.join(reactPath, "index.html"));
+});
+
+// any server errors cause this function to run
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  if (status === 500) {
+    // 500 means Internal Server Error
+    console.log("The server errored when processing a request!");
+    console.log(err);
+  }
+
+  res.status(status);
+  res.send({
+    status: status,
+    message: err.message,
+  });
+});
+
+// hardcode port to 3000 for now
+const port = process.env.PORT || 3000;
+const server = http.Server(app);
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
