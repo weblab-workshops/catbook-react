79ee45b4163ff149b270d8a2c4628a26b429de84
mercyo12 <119873550+mercyo12@users.noreply.github.com>
2023-01-13 13:39:35 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
+const snakeSpeed = 1000/5 # the number of times we update per second
 const main = () => {
     update();
     draw();
 }
 
-setInterval(main);
+setInterval(main, 1000/5);
 
 const update = () => {
+    console.log("updating")
 }
 
 const draw = () => {
