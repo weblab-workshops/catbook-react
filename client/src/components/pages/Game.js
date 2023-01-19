e84a189e67e58f0b1f99a007a01be229a746947b
Nicholas Tsao <ntt1729@gmail.com>
2023-01-18 21:01:27 -0500

    converted canvas to use useRef instead of doc getElementById

--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -8,6 +8,8 @@ import "../../utilities.css";
 import "./Game.css";
 
 const Game = (props) => {
+  const canvasRef = useRef(null);
+
   // add event listener on mount
   useEffect(() => {
     // TODO (Step 3.3): add event listener when the page is loaded (1 line)
@@ -34,7 +36,7 @@ const Game = (props) => {
   }, []);
 
   const processUpdate = (update) => {
-    drawCanvas(update);
+    drawCanvas(update, canvasRef);
   };
 
   // display text if the player is not logged in
@@ -47,7 +49,7 @@ const Game = (props) => {
     <>
       <div>
         {/* important: canvas needs id to be referenced by canvasManager */}
-        <canvas id="game-canvas" width="500" height="500" />
+        <canvas ref={canvasRef} width="500" height="500" />
         {loginModal}
       </div>
     </>
