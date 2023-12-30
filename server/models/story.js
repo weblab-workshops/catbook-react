19b3cec941a3d7c071808abdea6a1ba4a5152906
andrew <andrewliu278@gmail.com>
2023-12-30 16:09:29 -0600

    add creator_id comment

--- server/models/story.js
+++ server/models/story.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 //define a story schema for the database
 const StorySchema = new mongoose.Schema({
+  // TODO: add creator_id
   creator_name: String,
   content: String,
 });
