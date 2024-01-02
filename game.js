55e70bdb3c829d84a5829abfd4d38e70786f73f3
Enrique <enriquec@mit.edu>
2024-01-01 20:40:02 -0500

    Change indent to 2-space

--- game.js
+++ game.js
@@ -1,19 +1,21 @@
-const gameBoard = document.getElementById("game-board");
+const SNAKE_SPEED = 5;
+
+const gameBoard = document.getElementById('game-board');
 
 const main = () => {
-    update();
-    draw();
-}
+  update();
+  draw();
+};
 
-setInterval(main, 1000/SNAKE_SPEED);
+setInterval(main, 1000 / SNAKE_SPEED);
 
 const update = () => {
-    console.log("Updating");
-    updateSnake();
-}
+  console.log('Updating');
+  updateSnake();
+};
 
 const draw = () => {
-    gameBoard.innerHTML = "";
-    drawSnake(gameBoard);
-    drawFood(gameBoard);
-}
+  gameBoard.innerHTML = '';
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+};
--- snake.js
+++ snake.js
@@ -1,32 +1,31 @@
-const SNAKE_SPEED = 5;
 const snakeBody = [
-    {x:11, y:11},
-    {x:11, y:10},
-    {x:11, y:9}
+  { x: 11, y: 11 },
+  { x: 11, y: 10 },
+  { x: 11, y: 9 },
 ];
 
 const updateSnake = () => {
-    // remove tail segment
-    snakeBody.pop();
+  // Remove tail segment
+  snakeBody.pop();
 
-    // add new head segment
-    const newHead = {...snakeBody[0]};
-    const snakeDirection = getInputDirection();
-    
-    newHead.x += snakeDirection.x;
-    newHead.y += snakeDirection.y;
+  // Add new head segment
+  const newHead = { ...snakeBody[0] };
+  const snakeDirection = getInputDirection();
 
-    snakeBody.unshift(newHead);
-}
+  newHead.x += snakeDirection.x;
+  newHead.y += snakeDirection.y;
+
+  snakeBody.unshift(newHead);
+};
 
 // Don't change this function!
 const drawSnake = (gameBoard) => {
-    for (let i = 0; i < snakeBody.length; i++) {
-        const segment = snakeBody[i];
-        const snakeElement = document.createElement("div");
-        snakeElement.style.gridRowStart = segment.y;
-        snakeElement.style.gridColumnStart = segment.x;
-        snakeElement.classList.add("snake");
-        gameBoard.appendChild(snakeElement);
-    }
-}
+  for (let i = 0; i < snakeBody.length; i++) {
+    const segment = snakeBody[i];
+    const snakeElement = document.createElement('div');
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    snakeElement.classList.add('snake');
+    gameBoard.appendChild(snakeElement);
+  }
+};
