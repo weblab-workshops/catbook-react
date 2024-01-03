c635fa29ce4cad8fe7ea675101cbe4142149e38a
Enrique <enriquec@mit.edu>
2024-01-03 16:51:49 -0500

    Added some hints to input.js

--- input.js
+++ input.js
@@ -1,1 +1,6 @@
 // TODO: Create input event listeners for controlling the snake
+// Hint: Create a "keydown" event listener
+// (https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener)
+// 
+// Hint: Take a look at the object returned from the event to figure out which
+// key has been pressed (https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key)
