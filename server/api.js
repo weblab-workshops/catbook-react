95ac0c9813e1d46fea3bc119d9604e919a31308f
Joyce <jeyoon@mit.edu>
2024-01-05 01:06:17 -0500

    changed myName and data to current staff

--- server/api.js
+++ server/api.js
@@ -11,6 +11,30 @@ const express = require("express");
 
 const router = express.Router();
 
+// we haven't set up user login yet, so just
+// use a hardcoded name for now
+// TODO (step2) change to a unique name for workshop
+const myName = "Anonymous";
+
+const data = {
+  stories: [
+    {
+      _id: 0,
+      creator_name: "Tony Cui",
+      content: "Send it or blend it?",
+    },
+    // TODO (step1) Add new story!
+  ],
+  comments: [
+    {
+      _id: 0,
+      creator_name: "Stanley Zhao",
+      parent: 0,
+      content: "Both!",
+    },
+  ],
+};
+
 // an example GET route
 router.get("/test", (req, res) => {
   res.send({ message: "Wow I made my first API!" });
