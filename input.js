7e6adc6f4167db57fcc78fec28f27b0d82d0410a
Enrique <enriquec@mit.edu>
2024-01-05 17:04:20 -0500

    Update comments for step 3

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
-// TODO: Create input event listeners for controlling the snake
+// TODO 3.1: Create input event listeners for controlling the snake
 // Hint: Create a "keydown" event listener
 // (https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener)
 // 
 // Hint: Take a look at the object returned from the event to figure out which
-// key has been pressed (https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key)
+// key has been pressed
+// (https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key)
+// List of key values:
+// https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
