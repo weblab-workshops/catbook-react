660ae653f2cb449fb04b164cdbcfd586c49da234
Enrique <enriquec@mit.edu>
2024-01-05 17:07:43 -0500

    Remove prevInputDirection, can just use old inputDirection value

--- input.js
+++ input.js
@@ -1,19 +1,17 @@
 let inputDirection = { x: 0, y: 1 };
-let lastInputDirection = { x: 0, y: 1 };
 
 window.addEventListener('keydown', (event) => {
-  if (event.key === 'ArrowUp' && lastInputDirection.x !== 0) {
+  if (event.key === 'ArrowUp' && inputDirection.x !== 0) {
     inputDirection = { x: 0, y: -1 };
-  } else if (event.key === 'ArrowDown' && lastInputDirection.x !== 0) {
+  } else if (event.key === 'ArrowDown' && inputDirection.x !== 0) {
     inputDirection = { x: 0, y: 1 };
-  } else if (event.key === 'ArrowRight' && lastInputDirection.y !== 0) {
+  } else if (event.key === 'ArrowRight' && inputDirection.y !== 0) {
     inputDirection = { x: 1, y: 0 };
-  } else if (event.key === 'ArrowLeft' && lastInputDirection.y !== 0) {
+  } else if (event.key === 'ArrowLeft' && inputDirection.y !== 0) {
     inputDirection = { x: -1, y: 0 };
   }
 });
 
 const getInputDirection = () => {
-  lastInputDirection = inputDirection;
   return inputDirection;
 };
