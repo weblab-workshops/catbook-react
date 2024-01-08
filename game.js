6f59b2c77d1311ffe4280ad372661cb1b40b8f23
Enrique <enriquec@mit.edu>
2024-01-07 19:42:57 -0500

    Updated the starter todo comments

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
-// TODO: Implement the game loop
 
 const main = () => {
   update();
   draw();
 };
 
+// TODO: Make the game loop run 5 times per second
+// Hint: Check the setInterval documentation for additional arguments
+// https://developer.mozilla.org/en-US/docs/Web/API/setInterval
 setInterval(main);
 
 const update = () => {
