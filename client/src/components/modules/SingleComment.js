ca1ee4002659b0a6c3478c0f130ef387d16ac895
Andy Jiang <andyjiang2016@gmail.com>
2024-01-11 01:42:17 -0500

    remove duplicate div

--- client/src/components/modules/SingleComment.js
+++ client/src/components/modules/SingleComment.js
@@ -11,11 +11,9 @@ import React from "react";
 const SingleComment = (props) => {
   return (
     <div className="Card-commentBody">
-        <div className="Card-commentBody">
-          <span className="u-bold">{props.creator_name}</span>
-          <span>{" | " + props.content}</span>
-        </div>
-      </div>
+      <span className="u-bold">{props.creator_name}</span>
+      <span>{" | " + props.content}</span>
+    </div>
   );
 };
 
