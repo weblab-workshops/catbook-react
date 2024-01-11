ee12163ef2d760553165090ea18e4bed5da3cac6
Andy Jiang <andyjiang2016@gmail.com>
2024-01-10 22:12:46 -0500

    oops more corrent router notation real

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -21,7 +21,7 @@ const App = () => {
       <div className="App-container">
         <Router>
           <Feed path="/" />
-          <Profile path="/profile" />
+          <Profile path="profile" />
           <NotFound default />
         </Router>
       </div>
