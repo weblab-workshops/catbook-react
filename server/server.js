1eedea61e3b85284c3b59579ea770397282453ec
kenctrl <kennethkchoi22@gmail.com>
2024-01-16 14:29:28 -0500

    re-update mongo srv

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ const socketManager = require("./server-socket");
 // Server configuration below
 // TODO change connection URL after setting up your own database
 const mongoConnectionURL =
-  "mongodb+srv://kenchoi:03CXHjMYM7xUYNh6@cluster0.xbb74.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://weblab:jAT4po55IAgYWQgR@catbook-ylndp.mongodb.net/test?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
 const databaseName = "Cluster0";
 
