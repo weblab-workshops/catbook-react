dfbc229e87d442ab53964402cc0de2ec83fe3a33
kenctrl <kennethkchoi22@gmail.com>
2024-01-16 14:38:30 -0500

    update srv

--- server/server.js
+++ server/server.js
@@ -35,9 +35,9 @@ const socketManager = require("./server-socket");
 // Server configuration below
 // TODO change connection URL after setting up your own database
 const mongoConnectionURL =
-  "mongodb+srv://weblab:jAT4po55IAgYWQgR@catbook-ylndp.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://kenchoi:03CXHjMYM7xUYNh6@cluster0.xbb74.mongodb.net/?retryWrites=true&w=majority";;
 // TODO change database name to the name you chose
-const databaseName = "catbook";
+const databaseName = "Cluster0";
 
 // connect to mongodb
 mongoose
