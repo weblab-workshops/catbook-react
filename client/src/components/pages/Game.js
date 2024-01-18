b86e10897a940baac698f8a1b194b5f6c7f19d88
Michael Kuoch <m.kuoch1@gmail.com>
2024-01-17 23:52:46 -0500

    close socket in cleanup function

--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -25,6 +25,9 @@ const Game = (props) => {
     socket.on("update", (update) => {
       processUpdate(update);
     });
+    return () => {
+      socket.off("update");
+    }
   }, []);
 
   const processUpdate = (update) => {
