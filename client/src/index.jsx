88baa8a34751cad2b4e0d71efec88f252b3a16dc
Daniel Hong <dxhong@mit.edu>
2024-12-18 14:16:56 -0500

    vite migration

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.js";
+import App from "./components/App";
 
 // renders React Component "Root" into the DOM element with ID "root"
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // allows for live updating
-module.hot.accept();
+// module.hot.accept();
