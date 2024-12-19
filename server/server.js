644805982b62c7cf7c8998450340b552b18feee6
Daniel Hong <dxhong@mit.edu>
2024-12-18 23:21:00 -0500

    vite migration

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@
 | - Sets up error handling in case something goes wrong when handling a request
 | - Actually starts the webserver
 */
+require('dotenv').config();
 
 // validator runs some basic checks to make sure you've set everything up correctly
 // this is a tool provided by staff, so you don't need to worry about it
@@ -34,8 +35,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your own database
-const mongoConnectionURL =
-  "mongodb+srv://kenchoi:03CXHjMYM7xUYNh6@cluster0.xbb74.mongodb.net/?retryWrites=true&w=majority";
+const mongoConnectionURL = process.env.mongoURL;
 // TODO change database name to the name you chose
 const databaseName = "Cluster0";
 
