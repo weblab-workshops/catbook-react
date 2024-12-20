babbb7b507021165dfd74ae8b132d7f2757ba6ce
Daniel Hong <dxhong@mit.edu>
2024-12-19 20:18:33 -0500

    updated dependencies

--- client/src/components/pages/Chatbook.jsx
+++ client/src/components/pages/Chatbook.jsx
@@ -3,6 +3,7 @@ import ChatList from "../modules/ChatList";
 import Chat from "../modules/Chat";
 import { socket } from "../../client-socket";
 import { get } from "../../utilities";
+import { useOutletContext } from "react-router-dom";
 
 import "./Chatbook.css";
 
@@ -11,28 +12,9 @@ const ALL_CHAT = {
   name: "ALL CHAT",
 };
 
-/**
- * Page component to display when at the "/chat" route
- *
- * Proptypes
- * @param {string} userId id of current logged in user
- */
-const Chatbook = (props) => {
-  /**
-   * @typedef UserObject
-   * @property {string} _id
-   * @property {string} name
-   */
-  /**
-   * @typedef MessageObject
-   * @property {UserObject} sender
-   * @property {string} content
-   */
-  /**
-   * @typedef ChatData
-   * @property {MessageObject[]} messages
-   * @property {UserObject} recipient
-   */
+const Chatbook = () => {
+
+  let props = useOutletContext();
 
   const [activeChat, setActiveChat] = useState({
     recipient: ALL_CHAT,
