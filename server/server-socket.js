45e468dc9451206a24c756cb835abcdf6160d32c
Daniel Hong <dxhong@mit.edu>
2024-12-19 22:41:49 -0500

    update dependencies

--- server/server-socket.js
+++ server/server-socket.js
@@ -6,7 +6,7 @@ const socketToUserMap = {}; // maps socket ID to user object
 const getAllConnectedUsers = () => Object.values(socketToUserMap);
 const getSocketFromUserID = (userid) => userToSocketMap[userid];
 const getUserFromSocketID = (socketid) => socketToUserMap[socketid];
-const getSocketFromSocketID = (socketid) => io.sockets.connected[socketid];
+const getSocketFromSocketID = (socketid) => io.sockets.sockets.get(socketid);
 
 const addUser = (user, socket) => {
   const oldSocket = userToSocketMap[user._id];
