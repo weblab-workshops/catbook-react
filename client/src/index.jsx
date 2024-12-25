393adc19f23c5fb5ad47a5a9580ebc14b06433db
Daniel Hong <dxhong@mit.edu>
2024-12-24 18:30:10 -0800

    fixed import bug

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,7 +5,6 @@ import Feed from "./components/pages/Feed";
 import NotFound from "./components/pages/NotFound";
 import Profile from "./components/pages/Profile";
 import Chatbook from "./components/pages/Chatbook";
-import Game from "./components/pages/Game";
 
 
 import {
@@ -13,7 +12,6 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
-  useNavigate
 } from 'react-router-dom'
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
