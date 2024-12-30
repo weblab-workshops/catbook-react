fab0f9427ee846ea923dfb982a8bd401644d3ffc
Daniel Hong <dxhong@mit.edu>
2024-12-30 14:14:05 -0800

    update dependencies

--- client/src/components/modules/NavBar.jsx
+++ client/src/components/modules/NavBar.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import { Link } from "@reach/router";
-import GoogleLogin, { GoogleLogout } from "react-google-login";
+import { Link } from "react-router-dom";
+import { GoogleLogin } from "@react-oauth/google";
 
 import "./NavBar.css";
-
-// This identifies your web application to Google's authentication service
-const GOOGLE_CLIENT_ID = "395785444978-7b9v7l0ap2h3308528vu1ddnt3rqftjc.apps.googleusercontent.com";
 
 /**
  * The navigation bar at the top of all pages. Takes no props.
@@ -35,20 +32,15 @@ const NavBar = (props) => {
           Profile
         </Link>
         {loggedIn ? (
-          <GoogleLogout
-            clientId={GOOGLE_CLIENT_ID}
-            buttonText="Logout"
-            onLogoutSuccess={handleLogout}
-            onFailure={(err) => console.log(err)}
-            className="NavBar-link NavBar-login"
-          />
+          <button className="NavBar-link NavBar-login u-inlineBlock" onClick={handleLogout}>
+            Sign out
+          </button>
         ) : (
           <GoogleLogin
-            clientId={GOOGLE_CLIENT_ID}
-            buttonText="Login"
+            text="signin_with"
             onSuccess={handleLogin}
             onFailure={(err) => console.log(err)}
-            className="NavBar-link NavBar-login"
+            containerProps= {{'className': "NavBar-link NavBar-login u-inlineBlock"}}
           />
         )}
       </div>
