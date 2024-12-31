7168bd5e6254ae595953d6b32332db9b018faa7d
Daniel Hong <dxhong@mit.edu>
2024-12-30 16:28:22 -0800

    update dependencies

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
+// TODO (step5): import Router, Profile, Feed and NotFound
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// This identifies your web application to Google's authentication service
+const GOOGLE_CLIENT_ID = "884615792154-63asahc9uepm1aflp9rvq7sq12pm1cg8.apps.googleusercontent.com";
+
+// TODO (step5): implement router
 
 // renders React Component "Root" into the DOM element with ID "root"
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// allows for live updating
-module.hot.accept();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    {/* <Profile /> */}
+    <App />
+    {/* TODO (step5): use Router to route between pages */}
+  </GoogleOAuthProvider>
+);
