7b9e5f5291e63240eb5db2eeda63cd3b46348583
Andy Jiang <acyjiang@mit.edu>
2024-12-31 17:19:46 -0600

    add _id prop to SingleStory component

--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -35,7 +35,7 @@ const Feed = () => {
   const hasStories = stories.length !== 0;
   if (hasStories) {
     storiesList = stories.map((storyObj) => (
-      <SingleStory creator_name={storyObj.creator_name} content={storyObj.content} />
+      <SingleStory _id="test_id" creator_name={storyObj.creator_name} content={storyObj.content} />
     ));
   } else {
     storiesList = <div>No stories!</div>;
