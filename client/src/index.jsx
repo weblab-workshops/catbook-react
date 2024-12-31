2292ff0d3fc9a0d346c4f0bf6ef1bdd1a5529d0c
Daniel Hong <dxhong@mit.edu>
2024-12-30 17:02:02 -0800

    removed auth

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,18 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 // TODO (step5): import Router, Profile, Feed and NotFound
 
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
-// This identifies your web application to Google's authentication service
-const GOOGLE_CLIENT_ID = "884615792154-63asahc9uepm1aflp9rvq7sq12pm1cg8.apps.googleusercontent.com";
-
 // TODO (step5): implement router
 
 // renders React Component "Root" into the DOM element with ID "root"
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-    {/* <Profile /> */}
+  <>
     <App />
     {/* TODO (step5): use Router to route between pages */}
-  </GoogleOAuthProvider>
+  </>
 );
