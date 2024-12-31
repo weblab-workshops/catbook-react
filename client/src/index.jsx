8392d2b956155f1c0bcf0d00ea5e59db315a5d2d
Daniel Hong <dxhong@mit.edu>
2024-12-30 16:28:22 -0800

    update dependencies

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,35 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import Feed from "./components/pages/Feed";
+import NotFound from "./components/pages/NotFound";
+import Profile from "./components/pages/Profile";
+
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  useNavigate
+} from 'react-router-dom'
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// This identifies your web application to Google's authentication service
+const GOOGLE_CLIENT_ID = "884615792154-63asahc9uepm1aflp9rvq7sq12pm1cg8.apps.googleusercontent.com";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route errorElement={<NotFound />} element={<App />}>
+      <Route path="/" element={<Feed />}/>
+      <Route path="/profile" element={<Profile />}/>
+    </Route>
+  )
+)
 
 // renders React Component "Root" into the DOM element with ID "root"
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// allows for live updating
-module.hot.accept();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <RouterProvider router={router} />
+  </GoogleOAuthProvider>
+);
