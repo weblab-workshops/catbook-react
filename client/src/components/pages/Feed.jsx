b2ec6db6428a92cfee873ab85e71f117e4d1cd4a
Andy Jiang <acyjiang@mit.edu>
2024-12-31 17:35:20 -0600

    propagate changes from step4 and implement addNewStory in this step

--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -1,24 +1,42 @@
 import React, { useState, useEffect } from "react";
-import { get } from "../../utilities";
 import SingleStory from "../modules/SingleStory";
 import { NewStory } from "../modules/NewPostInput";
 // TODO (step6): remove SingleStory import, import Card
 
 const Feed = () => {
-  // TODO (step1): define state to hold stories
   const [stories, setStories] = useState([]);
 
+  // updates the stories state so that the new story is added immediately
+  const addNewStory = (value) => {
+    setStories(stories.concat(value));
+  }
+
   useEffect(() => {
-    get("/api/stories").then((storyObjs) => {
-      setStories(storyObjs);
-    });
+    const story1 = {
+      _id: "id1",
+      creator_name: "person1",
+      content: "story1",
+    };
+    const story2 = {
+      _id: "id2",
+      creator_name: "person2",
+      content: "story2",
+    };
+    const story3 = {
+      _id: "id3",
+      creator_name: "person3",
+      content: "story3",
+    };
+    const hardcodedStories = [story1, story2, story3];
+    
+    setStories(hardcodedStories);
   }, []);
 
   let storiesList = null;
   const hasStories = stories.length !== 0;
   if (hasStories) {
     storiesList = stories.map((storyObj) => (
-      <SingleStory creator_name={storyObj.creator_name} content={storyObj.content} />
+      <SingleStory _id="test_id" creator_name={storyObj.creator_name} content={storyObj.content} />
     ));
   } else {
     storiesList = <div>No stories!</div>;
@@ -26,11 +44,11 @@ const Feed = () => {
 
   return (
     <div>
-      <NewStory />
+      <NewStory addNewStory={addNewStory}/>
       {storiesList}
     </div>
   );
-  // TODO (step6): use Card instead of SingleStory
+  // TODO (step6): use Card instead of SingleStory, passing down the same props
 };
 
 export default Feed;
