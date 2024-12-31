e2ad153b9439f08f48b29fa8fc5145579af01492
Andy Jiang <acyjiang@mit.edu>
2024-12-31 17:06:36 -0600

    remove backend and hardcode stories

--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -1,24 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { get } from "../../utilities";
+import React, {useState, useEffect} from "react";
 // TODO (step2): import SingleStory
 // TODO (step4): import NewStory
 // TODO (step6): remove SingleStory import, import Card
 
 const Feed = () => {
-  // TODO (step1): define state to hold stories
   const [stories, setStories] = useState([]);
 
+  // TODO (step4): implement a callback function addNewStory that adds a 
+  // new story to the stories state
+  
+
   useEffect(() => {
-    get("/api/stories").then((storyObjs) => {
-      setStories(storyObjs);
-    });
+    const story1 = {
+      _id: "id1",
+      creator_name: "person1",
+      content: "story1",
+    };
+    const story2 = {
+      _id: "id2",
+      creator_name: "person2",
+      content: "story2",
+    };
+    const story3 = {
+      _id: "id3",
+      creator_name: "person3",
+      content: "story3",
+    };
+    const hardcodedStories = [story1, story2, story3];
+    
+    setStories(hardcodedStories);
   }, []);
 
   return <div>{JSON.stringify(stories)}</div>;
   // TODO (step2): render a SingleStory with hardcoded props
   // TODO (step3): map the state to SingleStory components
-  // TODO (step4): add in the NewStory component
-  // TODO (step6): use Card instead of SingleStory
+  // TODO (step4): add in the NewStory component and pass down addStory as a prop
+  // TODO (step6): use Card instead of SingleStory, passing down the same props
 };
 
 export default Feed;
