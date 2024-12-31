02088557d80a99a38eccc5085084d704190ea9b8
Daniel Hong <dxhong@mit.edu>
2024-12-30 16:52:22 -0800

    update dependencies

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
 // renders React Component "Root" into the DOM element with ID "root"
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// allows for live updating
-module.hot.accept();
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
