f86dc1b7156c1d22e2ebf07f67bfaea6adfc8c51
Daniel Hong <dxhong@mit.edu>
2025-01-01 11:51:39 -0800

    finished step2

--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -9,7 +9,9 @@ const Feed = () => {
   // updates the stories state so that the new story is added immediately
   const addNewStory = (value) => {
     // TODO (step2): post the new story to the server
-    setStories(stories.concat(value));
+    post("/api/story", value).then((storyObj) => {
+      setStories([storyObj].concat(stories));
+    });
   }
 
   useEffect(() => {
