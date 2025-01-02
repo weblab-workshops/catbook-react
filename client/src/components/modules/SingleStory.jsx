5f884fb67c1c2a899aeaae7b086558883d69b61e
Evan Kim <evan.e.kim@gmail.com>
2025-01-02 14:18:20 -0800

    swap steps 2 and 2

--- client/src/components/modules/SingleStory.jsx
+++ client/src/components/modules/SingleStory.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-// TODO (step2): import Card.css
+import "./SingleStory.css";
 
 /**
  * Story is a component that renders creator and content of a story
@@ -10,11 +9,11 @@ import React from "react";
  * @param {string} creator_name
  * @param {string} content of the story
  */
-const SingleStory = () => {
+const SingleStory = (props) => {
   return (
     <div className="Card-story">
-      This is a SingleStory!
-      {/* TODO (step2): use JSX and props to render story creator and content */}
+        <span className="u-bold">{props.creator_name}</span>
+        <p className="Card-storyContent">{props.content}</p>
     </div>
   );
 };
--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -1,37 +1,20 @@
 import React, {useState, useEffect} from "react";
-// TODO (step2): import SingleStory
+import SingleStory from "../modules/SingleStory";
 // TODO (step4): import NewStory
 // TODO (step6): remove SingleStory import, import Card
 
 const Feed = () => {
-  const [stories, setStories] = useState([]);
+  // TODO (step2): define a state called "stories" to hold stories
 
   // TODO (step4): implement a callback function addNewStory that adds a 
   // new story to the stories state
   
 
   useEffect(() => {
-    const story1 = {
-      _id: "id1",
-      creator_name: "person1",
-      content: "story1",
-    };
-    const story2 = {
-      _id: "id2",
-      creator_name: "person2",
-      content: "story2",
-    };
-    const story3 = {
-      _id: "id3",
-      creator_name: "person3",
-      content: "story3",
-    };
-    const hardcodedStories = [story1, story2, story3];
-    
-    setStories(hardcodedStories);
+    // TODO (step2): assign HARDCODED dummy values to the stories state 
   }, []);
 
-  return <div>{JSON.stringify(stories)}</div>;
+  return <SingleStory _id="test_id" creator_name="Evan" content="test" />;
   // TODO (step2): render a SingleStory with hardcoded props
   // TODO (step3): map the state to SingleStory components
   // TODO (step4): add in the NewStory component and pass down addStory as a prop
