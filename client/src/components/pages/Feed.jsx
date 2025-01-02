4ead1ef7ba84f479c88ba108a0c9626b33f36bab
Lucas Bautista <lucas_b@mit.edu>
2025-01-01 21:56:26 -0800

    Changed comments and stories to be more specific

--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -16,18 +16,18 @@ const Feed = () => {
     // TODO (step1): fetch the stories from the server
     const story1 = {
       _id: "id1",
-      creator_name: "person1",
-      content: "story1",
+      creator_name: "Stanley Zhao",
+      content: "Hi everyone",
     };
     const story2 = {
       _id: "id2",
-      creator_name: "person2",
-      content: "story2",
+      creator_name: "Abby Chou",
+      content: "Web.lab rocks",
     };
     const story3 = {
       _id: "id3",
-      creator_name: "person3",
-      content: "story3",
+      creator_name: "Andy Jiang",
+      content: "I like cats",
     };
     const hardcodedStories = [story1, story2, story3];
     
