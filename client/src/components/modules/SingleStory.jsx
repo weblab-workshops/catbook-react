66b461d75076e664ed5cc5db4ba4fef4973d6b66
Evan Kim <evan.e.kim@gmail.com>
2025-01-02 14:18:54 -0800

    change comment

--- client/src/components/modules/SingleStory.jsx
+++ client/src/components/modules/SingleStory.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * @param {string} creator_name
  * @param {string} content of the story
  */
-const SingleStory = () => {
+const SingleStory = (props) => {
   return (
     <div className="Card-story">
       This is a SingleStory!
