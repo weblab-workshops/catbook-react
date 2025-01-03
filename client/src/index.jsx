8ddecb2c92b7546a03cdd219360e36bfea7a6df1
Sophie Wang <sophie.ling.wang@gmail.com>
2025-01-03 16:28:44 -0700

    fixed router passing

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -21,8 +21,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route errorElement={<NotFound />} element={<App />}>
       <Route path="/" element={<Feed />} />
-      {/* TODO: change route to accept userId */}
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/profile/:userId" element={<Profile />} />
     </Route>
   )
 );
