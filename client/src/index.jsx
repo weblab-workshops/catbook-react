0c7152000851bd5dc677064ba3a135dc4bcb23df
Sophie Wang <sophie.ling.wang@gmail.com>
2025-01-03 16:18:38 -0700

    updated google client id

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,27 +5,26 @@ import Feed from "./components/pages/Feed";
 import NotFound from "./components/pages/NotFound";
 import Profile from "./components/pages/Profile";
 
-
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-  useNavigate
-} from 'react-router-dom'
+  useNavigate,
+} from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // This identifies your web application to Google's authentication service
-const GOOGLE_CLIENT_ID = "884615792154-63asahc9uepm1aflp9rvq7sq12pm1cg8.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "652128607404-rkv0o9ma7cqrlf75vlums1ga44ha59d9.apps.googleusercontent.com";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route errorElement={<NotFound />} element={<App />}>
-      <Route path="/" element={<Feed />}/>
-      <Route path="/profile" element={<Profile />}/>
+      <Route path="/" element={<Feed />} />
+      <Route path="/profile" element={<Profile />} />
     </Route>
   )
-)
+);
 
 // renders React Component "Root" into the DOM element with ID "root"
 ReactDOM.createRoot(document.getElementById("root")).render(
