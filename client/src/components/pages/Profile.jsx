31dc80d65641d21f8272423893ca40dcf4281127
Sophie Wang <sophie.ling.wang@gmail.com>
2025-01-03 19:55:00 -0700

    useparams

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import CatHappiness from "../modules/CatHappiness";
-
 import { get, post } from "../../utilities";
 import "./Profile.css";
 
