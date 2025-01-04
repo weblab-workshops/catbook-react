bfe6c7b913a9c4caf848f8a4c0dbee9b6bf87a8f
Sophie Wang <sophie.ling.wang@gmail.com>
2025-01-03 20:00:59 -0700

    useparams

--- client/src/components/modules/NavBar.jsx
+++ client/src/components/modules/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { GoogleLogin } from "@react-oauth/google";
 
 import { get, post } from "../../utilities";
--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import CatHappiness from "../modules/CatHappiness";
 
 import { get, post } from "../../utilities";
