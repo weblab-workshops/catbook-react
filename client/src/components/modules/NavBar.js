ac45434a356e064f39f40eda604065f179c96aeb
Sasha Krigel <sasha.krigel@gmail.com>
2025-12-21 12:09:41 -0600

    removing LLM page for now

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -32,9 +32,6 @@ const NavBar = (props) => {
         <Link to="/game/" className="NavBar-link">
           Game
         </Link>
-        <Link to="/llm/" className="NavBar-link">
-          LLM
-        </Link>
         {props.userId ? (
           <GoogleLogout
             clientId={GOOGLE_CLIENT_ID}
