2849e29dbcb168deaf3d4d993f9f8c174cbef424
angelofdesth <siyupeng.angeline@gmail.com>
2025-12-29 11:16:40 -0700

    added imports that students will need

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import CatHappiness from "../modules/CatHappiness";
 
+import { get } from "../../utilities"; 
 import "../../utilities.css";
 import "./Profile.css";
 
