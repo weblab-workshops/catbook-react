ba57be2eca4857e16cf5757d20b70549b21277aa
Thu Le <thule.hhs@gmail.com>
2025-12-29 21:54:34 -0800

    removed future step comments

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import NavBar from "./modules/NavBar";
 import Feed from "./pages/Feed";
-// TODO (step5): import Outlet
 
 // To use styles, import the necessary CSS files
 import "../utilities.css";
@@ -19,7 +18,6 @@ import "./App.css";
       <div className="App-container">
         {/* <Profile /> */}
         <Feed />
-        {/* TODO (step5): use Outlet to route between pages */}
       </div>
     </>
   );
--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -1,17 +1,12 @@
 import React, {useState, useEffect} from "react";
 import SingleStory from "../modules/SingleStory";
-// TODO (step4): import NewStory
-// TODO (step6): remove SingleStory import, import Card
 
 const Feed = () => {
-  // TODO (step2): define a state called "stories" to hold stories
-
-  // TODO (step4): implement a callback function addNewStory that adds a 
-  // new story to the stories state
+  // TODO (Step 2): define a state called "stories" to hold stories
   
 
   useEffect(() => {
-    // TODO (step2): assign HARDCODED dummy values to the stories state
+    // TODO (Step 2): assign HARDCODED dummy values to the stories state
     // a story should be an object of the form: 
     // {
     //   _id: "some random string of letters",
@@ -21,11 +16,8 @@ const Feed = () => {
     
   }, []);
 
+  // TODO (Step 2): render a SingleStory with hardcoded props
   return <SingleStory _id="test_id" creator_name="Evan" content="test" />;
-  // TODO (step2): render a SingleStory with hardcoded props
-  // TODO (step3): map the state to SingleStory components
-  // TODO (step4): add in the NewStory component and pass down addStory as a prop
-  // TODO (step6): use Card instead of SingleStory, passing down the same props
 };
 
 export default Feed;
